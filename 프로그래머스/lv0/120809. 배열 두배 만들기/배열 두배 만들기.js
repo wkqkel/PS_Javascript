9b20a5c016e36b8275fe5c25f5cc99805744ed92
Sangwon Park <wkqkel@naver.com>
2022-12-18 05:42:00 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    // const answer =[]
+    // for(let x of numbers){
+    //     answer.push(x*2)
+    // }
+    // return answer
+    
+    return numbers.map(num=>num*2)
+}
