4cd8a749a3a61e18579ff7b6a99499ac3d4f7742
Sangwon Park <wkqkel@naver.com>
2022-12-18 05:52:50 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,14 +1,22 @@
-function getGCD(a, b){
-    // 최대공약수: 두 수를 나눴을 때 나머지가 0이면 작은 값 아니면 작은값과 나머지를 가지고 재귀.
-   return a%b ? getGCD(b, a%b) : b
+function solution(n){
+    let pizza = 1;
+    while (pizza * 6 % n) {
+        pizza++;
+    }
+    return pizza;
 }
 
-function getLCM(a, b) {
+// function getGCD(a, b){
+//     // 최대공약수: 두 수를 나눴을 때 나머지가 0이면 작은 값 아니면 작은값과 나머지를 가지고 재귀.
+//    return a%b ? getGCD(b, a%b) : b
+// }
+
+// function getLCM(a, b) {
     
-    // 최소공배수 :두 수를 곱한 값 / 최대공약수
-   return a * b  / getGCD(a,b) 
-}
-function solution(n) {
-    //  n과 6의 최소공배수 / 6
-    return  getLCM(6, n)  / 6 
-}
+//     // 최소공배수 :두 수를 곱한 값 / 최대공약수
+//    return a * b  / getGCD(a,b) 
+// }
+// function solution(n) {
+//     //  n과 6의 최소공배수 / 6
+//     return  getLCM(6, n)  / 6 
+// }
