2e165d6102274e6d5a8e5c98090de2769a7ebf2e
Sangwon Park <wkqkel@naver.com>
2022-12-21 08:12:43 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,3 +1,4 @@
 function solution(order) {
- return [...(order+"")].filter(item=>[3,6,9].includes(+item)).length
+ // return [...(order+"")].filter(item=>[3,6,9].includes(+item)).length
+    return (order+"").replaceAll(/[^369]/g,'').length
 }
