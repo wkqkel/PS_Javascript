5208ebd1970d72cd4e734e11a8f1010bfdbd0df5
Sangwon Park <wkqkel@naver.com>
2022-12-23 01:08:36 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,5 +1,6 @@
 function solution(s1, s2) {
-    let ans = 0 
-    for (let x of s1) if(s2.includes(x)) ans++
-    return ans
+    // let ans = 0 
+    // for (let x of s1) if(s2.includes(x)) ans++
+    // return ans
+    return s1.filter(x=> s2.includes(x)).length
 }
