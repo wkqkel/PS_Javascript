7d3e1da55133da590b3c9e0dd6a18aaa0d2d6347
Sangwon Park <wkqkel@naver.com>
2022-12-23 07:45:41 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,3 +1,4 @@
 function solution(num, k) {
-   return (num+'').indexOf(k) >-1 ?  (num+'').indexOf(k) + 1 : -1
+   // return (num+'').indexOf(k) >-1 ?  (num+'').indexOf(k) + 1 : -1
+    return (num+'').indexOf(k) + 1 || -1
 }
