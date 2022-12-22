5320ecb9088b3424cc920dbdd7e581c8657c5f3c
Sangwon Park <wkqkel@naver.com>
2022-12-22 09:18:43 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,4 +1,8 @@
 function solution(s) {
-    const sorted = [...s].sort()
-    return sorted.filter((e,i,list)=> e!==list[i+1] && e!==list[i-1]).join('')
+    return  [...s].sort()
+                  .filter((e,i,list)=> e!==list[i+1] && e!==list[i-1])
+                  .join('')
+    // let ans = []
+    // for(let x of s) if(s.indexOf(x) === s.lastIndexOf(x)) ans.push(x)
+    // return ans.sort().join('')
 }
