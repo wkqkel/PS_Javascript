37763a25d7d681bcfb73711525773b613195e2f1
Sangwon Park <wkqkel@naver.com>
2022-12-23 21:06:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.21 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,9 @@
 function solution(participant, completion) {
-  participant.sort()
-  completion.sort()
-  for(let i=0; i<participant.length; i++){
-    if(participant[i]!==completion[i]) return participant[i];
-  }
+  // participant.sort()
+  // completion.sort()
+  // for(let i=0; i<participant.length; i++){
+  //   if(participant[i]!==completion[i]) return participant[i];
+  // }
+ return participant.find((v)=>!completion[v]--, 
+                 completion.map(v=>completion[v] = (completion[v] | 0)+1 ))
 }
