c337b1d870a47a440c38605caa6c1be3adbd5ec5
Sangwon Park <wkqkel@naver.com>
2022-12-23 22:41:44 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,8 @@
 function solution(nums) {
-    const map = new Map();
-    for (let x of nums){
-        map.set(x, (map.get(x)||0) + 1 );
-    }
-    
-    return Math.min(map.size, nums.length/2)
+    // const map = new Map();
+    // for (let x of nums){
+    //     map.set(x, (map.get(x)||0) + 1 );
+    // }
+    // return Math.min(map.size, nums.length/2)
+    return Math.min(new Set(nums).size, nums.length/2)
 }
