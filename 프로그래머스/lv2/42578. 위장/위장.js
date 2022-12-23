76314f2eb576916cb9466b80d3aa20c7b70937ba
Sangwon Park <wkqkel@naver.com>
2022-12-24 08:54:49 +0900

    [level 2] Title: 위장, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -0,0 +1,10 @@
+function solution(clothes) {
+    const map = new Map()
+    clothes.map((item)=> {
+        const data = map.get(item[1]) || []
+        map.set(item[1], [...data,item[0]])
+    })
+    return [...map.values()]
+                .reduce((acc,cur)=> acc * ( cur.length + 1),1) - 1
+   
+}
