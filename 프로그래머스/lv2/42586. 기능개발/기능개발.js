0ed530b2ca9f2dcbbe03caab37946aac2ac9a526
Sangwon Park <wkqkel@naver.com>
2022-12-24 11:02:58 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,15 +1,11 @@
 function solution(progresses, speeds) {
-
-
     const ans = {date:0, arr:[]}
     const arr = progresses.map((v,i)=> (Math.ceil((100-v)/speeds[i]))) 
     for(let v of arr){
-        if(ans.date >= v){
-            ans.arr[ans.arr.length -1] += 1
-        } else {
-            ans.arr.push(1)
-        }
-        ans.date = Math.max(ans.date, v)
+       ans.date >= v 
+            ?ans.arr[ans.arr.length -1] += 1
+            :ans.arr.push(1)
+       ans.date = Math.max(ans.date, v)
     }
     return ans.arr
 }
