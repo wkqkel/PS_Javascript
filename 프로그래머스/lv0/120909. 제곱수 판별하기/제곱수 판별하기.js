38964401790e4fee28e3894c83ed9e6a8f62076a
Sangwon Park <wkqkel@naver.com>
2022-12-25 09:09:19 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,4 +1,5 @@
 function solution(n) {
-    const sqrt = Math.sqrt(n)
-   return !(sqrt === Math.floor(sqrt)) + 1
+   //  const sqrt = Math.sqrt(n)
+   // return !(sqrt === Math.floor(sqrt)) + 1
+    return !Number.isInteger(Math.sqrt(n)) + 1
 }
