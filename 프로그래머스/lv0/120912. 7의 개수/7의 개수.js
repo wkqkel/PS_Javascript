80e962a518b15b0bcd4f5fdfc10a5cfcad91f7a0
Sangwon Park <wkqkel@naver.com>
2022-12-25 10:44:50 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,3 +1,4 @@
 function solution(array) {
-   return [...array.join('')].filter(v=> v==='7').length
+   // return [...array.join('')].filter(v=> v==='7').length
+    return array.join('').split('7').length - 1
 }
