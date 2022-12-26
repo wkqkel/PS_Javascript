a5bef0466745d84b870e70718b41608cc7185772
Sangwon Park <wkqkel@naver.com>
2022-12-26 11:52:15 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,6 +1,9 @@
 function solution(my_string) {
-    return my_string
-            .replace(/[^0-9]/g, ' ')
-            .split(' ')
-            .reduce((acc,cur)=> acc + (+cur||0),0)
+    // return my_string
+    //         .replace(/[^0-9]/g, ' ')
+    //         .split(' ')
+    //         .reduce((acc,cur)=> acc + (+cur||0),0)
+    
+    // \d+는 '하나 혹은 그 이상 연결된 숫자 `
+    return my_string.split(/\D+/).reduce((acc,cur)=> acc + +cur, 0)
 }
