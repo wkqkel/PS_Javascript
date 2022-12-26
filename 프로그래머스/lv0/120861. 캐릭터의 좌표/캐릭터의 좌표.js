46b55690d429a8eb63ef40afe05fdb1d23dbc8b7
Sangwon Park <wkqkel@naver.com>
2022-12-26 10:39:18 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.14 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,18 @@
+const KEY_INPUT_MAP = {
+    up: [0,1],
+    down: [0, -1],
+    left: [-1,0],
+    right: [1,0]
+}
+function solution(keyinput, board) {
+  const [maxX, maxY] = board.map((v)=> ~~(v/2))
+
+  return keyinput
+      .map((v)=>KEY_INPUT_MAP[v])
+      .reduce((acc,cur)=> {
+        const x = acc[0]+cur[0] > 0 
+        ? Math.min(acc[0]+cur[0],maxX) : Math.max(acc[0]+cur[0], -maxX) 
+        const y = acc[0]+cur[0] > 0 
+        ? Math.min(acc[1]+cur[1],maxY) : Math.max(acc[1]+cur[1], -maxY) 
+        return [x,y]}, [0,0])
+}
