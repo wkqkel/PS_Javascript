38971cb95c33103772a88164ed814951002e8f1f
Sangwon Park <wkqkel@naver.com>
2022-12-26 11:33:17 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,7 +1,9 @@
 function solution(numbers) {
-  const positiveArr= [...numbers].sort((a,b)=> b-a);
-  const negativeArr = [...numbers].filter(v=>v<0).sort((a,b)=> a-b);
+//   const positiveArr= [...numbers].sort((a,b)=> b-a);
+//   const negativeArr = [...numbers].filter(v=>v<0).sort((a,b)=> a-b);
     
-  return Math.max(positiveArr[0]*positiveArr[1],
-                  (negativeArr[0]*negativeArr[1])|| Number.MIN_SAFE_INTEGER )
+//   return Math.max(positiveArr[0]*positiveArr[1],
+//                   (negativeArr[0]*negativeArr[1])|| Number.MIN_SAFE_INTEGER )
+    numbers.sort((a,b)=> b-a)
+    return Math.max(numbers[0] * numbers[1], numbers.at(-1) * numbers.at(-2))
 }
