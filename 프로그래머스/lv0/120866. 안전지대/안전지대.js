9ba7b4589533649225094675688711ceff54b726
Sangwon Park <wkqkel@naver.com>
2022-12-27 23:01:01 +0900

    [level 0] Title: 안전지대, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,20 +1,23 @@
-
+const AROUND = [[-1,-1],[-1,0],[-1,1],
+                [0,-1],[0,0],[0,1],
+                [1,-1],[1,0],[1,1]]
 function solution(board) {
     let res = 0
-  
-    for(let i = 0; i< board.length; i++){
-        for(let j= 0; j< board[i].length; j++){
-            if((!board[i][j] 
-               && !board[i-1]?.[j-1]
-               && !board[i-1]?.[j]
-               && !board[i-1]?.[j+1]
-               && !board[i][j-1]
-               && !board[i][j+1]
-               && !board[i+1]?.[j-1]
-               && !board[i+1]?.[j]
-               && !board[i+1]?.[j+1])) res++
-        }
-    }
+    board.map((row,y)=> row.map((it, x) => !AROUND.some(([ox,oy])=> board[oy+y]?.[ox+x]) && res++))
+
+    // for(let i = 0; i< board.length; i++){
+    //     for(let j= 0; j< board[i].length; j++){
+    //         if((!board[i][j] 
+    //            && !(board[i-1]||[])[j-1]
+    //            && !(board[i-1]||[])[j]
+    //            && !board[i-1]?.[j+1]
+    //            && !board[i][j-1]
+    //            && !board[i][j+1]
+    //            && !board[i+1]?.[j-1]
+    //            && !board[i+1]?.[j]
+    //            && !board[i+1]?.[j+1])) res++
+    //     }
+    // }
  
     return res
 }
