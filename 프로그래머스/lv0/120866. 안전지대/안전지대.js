461bb0843190116460816ba48dbaa9fbbc5e59f4
Sangwon Park <wkqkel@naver.com>
2022-12-27 22:52:04 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -5,14 +5,14 @@ function solution(board) {
     for(let i = 0; i< board.length; i++){
         for(let j= 0; j< board[i].length; j++){
             if((!board[i][j] 
-               && !(board[i-1]||[])[j-1]
-               && !(board[i-1]||[])[j]
-               && !(board[i-1]||[])[j+1]
+               && !board[i-1]?.[j-1]
+               && !board[i-1]?.[j]
+               && !board[i-1]?.[j+1]
                && !board[i][j-1]
                && !board[i][j+1]
-               && !(board[i+1]||[])[j-1]
-               && !(board[i+1]||[])[j]
-               && !(board[i+1]||[])[j+1])) res++
+               && !board[i+1]?.[j-1]
+               && !board[i+1]?.[j]
+               && !board[i+1]?.[j+1])) res++
         }
     }
  
