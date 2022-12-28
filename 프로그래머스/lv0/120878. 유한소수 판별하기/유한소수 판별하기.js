33f6d89a7dc9c3e684bf5f187ce1d97fab38375e
Sangwon Park <wkqkel@naver.com>
2022-12-28 23:29:32 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    function makeIrreducibleFraction(a,b){
+        for(let i=a; a>1; i--){
+            if(a%i===0 && b%i===0) return b/i
+        }
+    }
+   const denominator = makeIrreducibleFraction(a,b)
+
+   for(let i=3; i<=denominator; i++){
+       if(i % 2 ===0 || i % 5 ===0) continue;
+       if(denominator % i === 0) return 2
+   }
+   return 1
+}
