a323f53045544f1334f5415e8fc60e1b6bbdf46e
Sangwon Park <wkqkel@naver.com>
2022-12-28 22:53:59 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,12 @@
+function solution(lines) {
+  // const ans = new Array(200).fill(0);
+  // lines.forEach(([a, b]) => {
+  //       for(a;a<b;a++)ans[a+100]+=1
+  // });
+  // return ans.filter(el=>el>1).length;
+  let ans = {}
+  lines.forEach(([a,b])=>{
+      for(a; a<b; a++) ans[a] = (ans[a]||0) +1;
+  })
+  return Object.values(ans).filter(el=>el>1).length;
+}
