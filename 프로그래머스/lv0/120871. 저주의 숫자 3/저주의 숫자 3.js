f6f5af1ba47c7fefdf9d0fadd539f514986bd597
Sangwon Park <wkqkel@naver.com>
2022-12-28 09:49:30 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,8 +1,7 @@
 function solution(n) {
-    let num=1;
-    for(let i = 1; i<=n; i++) {
-        num = i
-        if(num % 3 === 0 || (num + "").includes('3')) n++
+    let len = n
+    for(let i = 1; i<=len; i++) {
+        if(i % 3 === 0 || (i + "").includes('3')) len++
+        if(i===len) return i
     }
-    return num
 }
