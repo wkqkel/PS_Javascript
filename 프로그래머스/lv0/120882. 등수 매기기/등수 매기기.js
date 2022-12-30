940a825df34fdb35172905e4c6a291cabc7ffde5
Sangwon Park <wkqkel@naver.com>
2022-12-30 20:14:40 +0900

    [level 0] Title: 등수 매기기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,6 +1,10 @@
 function solution(score) {
-  const avrScoreArr = score.map(([eng,math])=> (eng+math)/2)
-  const sortedAvrScoreArr = [...avrScoreArr].sort((a,b)=> b-a)
-  return avrScoreArr.map(v=> sortedAvrScoreArr.indexOf(v)+1)
+  // const avrScoreArr = score.map(([eng,math])=> (eng+math)/2)
+  // const sortedAvrScoreArr = [...avrScoreArr].sort((a,b)=> b-a)
+  // return avrScoreArr.map(v=> sortedAvrScoreArr.indexOf(v)+1)
+ return score.map((v1)=> 
+          score.filter((v2)=> (v1[0]+v1[1])/2 < (v2[0]+v2[1])/2  ).length + 1)
+    
   
+
 }
