365be2e7e90ba9ecb3dc9f9b51097c811964ae96
Sangwon Park <wkqkel@naver.com>
2022-12-30 20:49:51 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,10 +1,12 @@
 function solution(id_pw, db) {
-    
-    let ans = 'fail'
-    db.map(([id,pw])=> {
-           if(id_pw[0]===id) ans='wrong pw'
-           if(id_pw[0]===id && id_pw[1]===pw) return ans='login'
-        }
-    )
-    return ans
+    // let ans = 'fail'
+    // db.map(([id,pw])=> {
+    //        if(id_pw[0]===id) ans='wrong pw'
+    //        if(id_pw[0]===id && id_pw[1]===pw) return ans='login'
+    //     }
+    // )
+    // return ans
+    const [id,pw] = id_pw
+    const map = new Map(db)
+    return map.has(id) ? (map.get(id) ===pw ? 'login':'wrong pw') : 'fail'
 }
