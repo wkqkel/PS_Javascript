d76cbaf7c7ec82cad8247edf569a7c28aec1431c
Sangwon Park <wkqkel@naver.com>
2022-12-30 19:50:36 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,3 +1,4 @@
 function solution(numlist, n) {
-  return numlist.sort((a,b)=>b-a).sort((a,b)=> Math.abs(n-a)-Math.abs(n-b))
+  // return numlist.sort((a,b)=>b-a).sort((a,b)=> Math.abs(n-a)-Math.abs(n-b))
+    return numlist.sort((a,b)=> Math.abs(n-a)-Math.abs(n-b) || b - a)
 }
