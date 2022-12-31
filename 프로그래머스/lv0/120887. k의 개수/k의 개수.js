1720f7c8b4ff51305ffae46a21b7bd620f7ba263
Sangwon Park <wkqkel@naver.com>
2022-12-31 13:00:28 +0900

    [level 0] Title: k의 개수, Time: 5.89 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,7 +1,12 @@
 function solution(i, j, k) {
-    let ans = 0
-    for(let cur=i; cur<=j; cur++){
-        ans += [...(cur+'')].filter(v=> +v===k).length
+    // let ans = 0
+    // for(let cur=i; cur<=j; cur++){
+    //     ans += [...(cur+'')].filter(v=> +v===k).length
+    // }
+    // return ans
+    let a = '';
+    for (i; i<=j; i++){
+        a += i;
     }
-    return ans
+    return a.split(k).length - 1
 }
