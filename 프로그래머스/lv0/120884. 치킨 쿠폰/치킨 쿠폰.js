39dd404c4197278b43d05c35ca8169429219e513
Sangwon Park <wkqkel@naver.com>
2022-12-31 13:34:47 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,10 +1,14 @@
 function solution(chicken) {
-    let coupons = chicken;
-    let service = 0;
-    while(Math.floor(coupons / 10)) {
-        let order = Math.floor(coupons / 10)
-        service += order;
-        coupons = coupons % 10 + order
-    }
-    return service
+//     let coupons = chicken;
+//     let service = 0;
+//     while(Math.floor(coupons / 10)) {
+//         let order = Math.floor(coupons / 10)
+//         service += order;
+//         coupons = coupons % 10 + order
+//     }
+//     return service
+    
+    // 이게모야..
+   return ~~(chicken*0.111111)
+   // return parseInt((chicken-1) / 9)
 }
