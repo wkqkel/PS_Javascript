ca9c2bcba9a89708f5e96458962f975d7852b646
Sangwon Park <wkqkel@naver.com>
2023-01-04 23:21:50 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -12,7 +12,7 @@ function solution(num, total) {
     
     // 1씩 증가하는 등차수열이므로 중간값에서 갯수/2를 빼거나 더하면 min과 max가 나옴.
     // 짝수일수있는데 이땐 중간값 두개중에 큰값에서 갯수/2 를 빼면 min 나옴. Math.ceil
+    // const max = Math.floor(total/num) + Math.floor(num/2)
     const min = Math.ceil(total/num) - Math.floor(num/2)
-    const max = Math.floor(total/num) + Math.floor(num/2)
     return Array(num).fill(min).map((v,i)=>v+i)
 }
