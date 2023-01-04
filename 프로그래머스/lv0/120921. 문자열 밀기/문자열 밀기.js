5a97454cfdec4a03167edfb18c1263f0736386c3
Sangwon Park <wkqkel@naver.com>
2023-01-04 23:50:58 +0900

    [level 0] Title: 문자열 밀기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,11 @@
+function solution(A, B) {
+
+  let count = 0;
+  let a = A
+  for (let i=0; i<a.length; i++) {
+      if(a===B) return count
+      a = a.slice(-1) + a.slice(0, -1)
+      count++
+  }
+  return -1
+}
