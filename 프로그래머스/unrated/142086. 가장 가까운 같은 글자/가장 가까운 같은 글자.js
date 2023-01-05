544c23097ff04965ab8ab632a07bc95b75c0b723
Sangwon Park <wkqkel@naver.com>
2023-01-05 23:23:13 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.45 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,3 +1,5 @@
 function solution(s) {
-    return [...s].map((v,i,l)=>l.indexOf(v)===i? -1 : i-l.lastIndexOf(v,i-1))
+    return [...s].map((v,i,l)=>l.indexOf(v)===i
+                      ? -1 
+                      : i-l.lastIndexOf(v,i-1))
 }
