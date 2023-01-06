03c59bdeee379b6fbcdab4fc2151f1ff0e8ec66a
Sangwon Park <wkqkel@naver.com>
2023-01-06 10:20:44 +0900

    [unrated] Title: 문자열 나누기, Time: 3.00 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -1,18 +1,27 @@
 function solution(s) {
-    let x = s[0]
-    let count = [0,0]
-    let ans = []
-    for(let i=0; i < s.length; i++) {
-        x===s[i] ? count[0]++ :count[1]++
-        const startIdx = ans.reduce((acc,cur)=> acc+cur.length,0)
-        if(count[0]===count[1]) {
-            ans.push(s.slice(startIdx,i+1))
-            count = [0,0]
-            x = s[i+1]
-        } else{
-            // 남은 문자는 그냥 마지막 하나만 들어가도 카운트이기때문에 성립한 것.
-            if(i===s.length-1) ans.push(s[i])
-        }
+    let first = ''
+    let ans = cnt = 0;
+    for (let x of s) {
+        if(!first) first = x;
+        cnt += first === x ? 1 : -1;
+        if(!cnt) first = '', ans++;
     }
-    return ans.length
+    return !!cnt + ans
+    
+    // let x = s[0]
+    // let count = [0,0]
+    // let ans = []
+    // for(let i=0; i < s.length; i++) {
+    //     x===s[i] ? count[0]++ :count[1]++
+    //     const startIdx = ans.reduce((acc,cur)=> acc+cur.length,0)
+    //     if(count[0]===count[1]) {
+    //         ans.push(s.slice(startIdx,i+1))
+    //         count = [0,0]
+    //         x = s[i+1]
+    //     } else{
+    //         // 남은 문자는 그냥 마지막 하나만 들어가도 카운트이기때문에 성립한 것.
+    //         if(i===s.length-1) ans.push(s[i])
+    //     }
+    // }
+    // return ans.length
 }
