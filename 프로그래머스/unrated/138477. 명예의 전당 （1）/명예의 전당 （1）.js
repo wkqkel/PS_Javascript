6c3f0864ece946685819ee822bd6bd62f36d89ed
Sangwon Park <wkqkel@naver.com>
2023-01-06 09:54:27 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,18 +1,27 @@
 function solution(k, score) {
-    const ans = [];
     const arr = [];
-    for(let i=0; i<score.length; i++){
-        if(arr.length < k) {
-            arr.push(score[i])
-            arr.sort((a,b)=>b-a);
-        } else {
-          const idx =  arr.indexOf(arr.find(v=>v<score[i]))
-          if(idx !== -1) {
-              arr.splice(idx, 0 , score[i])
-              arr.pop()
-          }
-        }
-        ans.push(arr.at(-1))
-    }
-    return ans
+    return score.map((v)=>{
+            arr.push(v);
+            arr.sort((a,b)=>a-b);
+            if(arr.length > k) arr.shift()
+            return arr[0];
+    })
+    
+    
+    // const ans = [];
+    // const arr = [];
+    // for(let i=0; i<score.length; i++){
+    //     if(arr.length < k) {
+    //         arr.push(score[i])
+    //         arr.sort((a,b)=>b-a);
+    //     } else {
+    //       const idx =  arr.indexOf(arr.find(v=>v<score[i]))
+    //       if(idx !== -1) {
+    //           arr.splice(idx, 0 , score[i])
+    //           arr.pop()
+    //       }
+    //     }
+    //     ans.push(arr.at(-1))
+    // }
+    // return ans
 }
