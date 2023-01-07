f6483695a684ec1d9d39c495e5473c72f34be568
Sangwon Park <wkqkel@naver.com>
2023-01-07 14:02:10 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.99 ms, Memory: 35.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,29 @@
+function getMeasureNumber(n){
+    let cnt = 0;
+    for (let i = 1; i * i <= n; i++) {
+        if(i * i === n) cnt++;
+        else if (n % i === 0) cnt +=2;
+    }
+    return cnt;
+}
+
+function solution(number, limit, power) {
+    
+    let ans = 0
+    for(let i=1; i<=number; i++){
+     const measureNumber = getMeasureNumber(i)
+     ans += measureNumber > limit ? power : measureNumber
+    }
+    return ans
+}
+
+// function getMeasureNumber(n){
+//    return new Array(n).fill(1).map((v,i)=> v+i).filter(v=> n%v===0).length
+// }
+
+// function solution(number, limit, power) {
+//     return Array(number).fill(1).map((v,i) => v+i)
+//         .reduce((acc,cur)=> {
+//         const measureNumber = getMeasureNumber(cur)
+//         return measureNumber > limit ? acc + power : acc + measureNumber }, 0)
+// }
