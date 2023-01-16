f4e2516e77c091d43fb67aab15b2b9fef2fff75d
Sangwon Park <wkqkel@naver.com>
2023-01-16 18:58:10 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -3,6 +3,7 @@ function solution(ingredient) {
     let cnt = 0
     for (const v of ingredient){
         stack.push(v)
+        // 전체문자열 또는 array를 조회하면 시간초과
         const last = stack.slice(-4).join('')
         if(last === '1231') {
             cnt++
@@ -12,14 +13,14 @@ function solution(ingredient) {
     return cnt
 }
 
-
-       //  let cnt = 0;
-       //  const hamburger = '1231';
-       //  let str = ingredient.join('')
-       //  let replace = str.replace(hamburger, '')
-       //  while(replace !== str){
-       //     cnt++
-       //     str = replace
-       //     replace = str.replace(hamburger, '')
-       //  }
-       // return  cnt
+//  아래처럼 replace로 하는게 includes보단 성능이 좋았음.
+//  let cnt = 0;
+//  const hamburger = '1231';
+//  let str = ingredient.join('')
+//  let replace = str.replace(hamburger, '')
+//  while(replace !== str){
+//     cnt++
+//     str = replace
+//     replace = str.replace(hamburger, '')
+//  }
+// return  cnt
