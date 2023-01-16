418b00a4cb18cd6b36ede48fdf0d7c4b1771ab64
Sangwon Park <wkqkel@naver.com>
2023-01-16 14:56:37 +0900

    [level 2] Title: H-Index, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,9 +1,13 @@
 function solution(citations) {
-    const { length } = citations
-
-    for(let i=length; i>0; i--){
-   
-      if(i <= citations.filter((v)=>v>=i).length) return i
+    // 논문들의 인용된 횟수가 h이상을 만족하는 논문수 h의 최댓값
+    // 최대 논문수는 citations.length
+    // 최대논문수에서 하나씩 빼가면서 
+    // citations.filter((v)=>v>=h).length 는 인용된 횟수가 h이상인 논문의 갯수 가
+    // 논문수 이상이면 해당 논문수가 정답
+    // 인용된 횟수가 0이면 0리턴
+    
+    for(let h=citations.length; h>0; h--){
+      if(h <= citations.filter((v)=>v>=h).length) return h
     }
     return 0
 }
