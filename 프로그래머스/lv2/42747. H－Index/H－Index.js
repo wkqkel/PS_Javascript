91d033e1c9f6cc687c2e08e0d90350731a51c28b
Sangwon Park <wkqkel@naver.com>
2023-01-16 21:42:40 +0900

    [level 2] Title: H-Index, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -5,7 +5,13 @@ function solution(citations) {
     // citations.filter((v)=>v>=h).length 는 인용된 횟수가 h이상인 논문의 갯수 가
     // 논문수 이상이면 해당 논문수가 정답
     
-    for(let h=citations.length; h>=0; h--){
-      if(h <= citations.filter((v)=>v>=h).length) return h
-    }
+    // for(let h=citations.length; h>=0; h--){
+    //   if(h <= citations.filter((v)=>v>=h).length) return h
+    // }
+    return citations.sort((a, b) => b-a).filter((v,i)=> v>=i+1).length
 }
+
+// https://velog.io/@numerok/프로그래머스-H-Index-자바스크립트
+// 쉬운 설명 참고
+// 6 5 3 1 0
+// 1 2 3 4 5
--- 프로그래머스/lv2/42747. H－Index/README.md
+++ 프로그래머스/lv2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.14 ms
+메모리: 33.6 MB, 시간: 0.08 ms
 
 ### 구분
 
