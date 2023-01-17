576f5121a9e0bdc153fa4adea44ef9b08a6afdf9
Sangwon Park <wkqkel@naver.com>
2023-01-17 11:41:03 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -13,7 +13,9 @@ function solution(a, b, n) {
 
     // Math.floor((n - b) / (a - b)) * b : n병 갖다주고 받은 병 갯수
     // Math.floor((n - b) / (a - b)) : a병을 몇 번 갖다줬는지
-    // n-b : 
     // a-b : 한번에 갖다주면서 손해보는 양
+    // n-b => a병을 몇 번 갖다줬는지인데 
+    // 한 번 갖다줘도 b만큼 더해지기 때문에(a-b)
+    // 초기값 b를 빼줘야 a번 갖다준 양이 나옴.
     return Math.floor((n - b) / (a - b)) * b
 }
