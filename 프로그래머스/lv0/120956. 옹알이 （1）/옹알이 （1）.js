bea3887e3115105ec431296b1343aa3329ad93e4
Sangwon Park <wkqkel@naver.com>
2023-01-17 16:21:14 +0900

    [level 0] Title: 옹알이 (1), Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -4,7 +4,8 @@ const ans =[]
 for(let i=0; i<babbling.length; i++){
     let item = babbling[i];   
     while(canArr.some(v=> item.startsWith(v))){
-       item = item.split(canArr.find(v=> item.startsWith(v)))[1] || ''
+       item = item.replace(canArr.find(v=> item.startsWith(v)),'')
+       // item = item.split(canArr.find(v=> item.startsWith(v)))[1] || ''
     }
     ans.push(item)
 }
