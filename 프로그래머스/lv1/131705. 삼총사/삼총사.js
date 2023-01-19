022ef5ce248d9cc2fc5436a9ce700eba48faf04d
Sangwon Park <wkqkel@naver.com>
2023-01-19 23:32:29 +0900

    [level 1] Title: 삼총사, Time: 0.42 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,17 +1,18 @@
-function combinations(arr, n) {
-  if (n === 1) return arr.map((v) => [v]);
-  const result = [];
-
-  arr.forEach((fixed, idx, arr) => {
-    const rest = arr.slice(idx + 1);
-    const combis = combinations(rest, n - 1);
-    const combine = combis.map((v) => [fixed, ...v]);
-    result.push(...combine);
-  });
-  return result;
+function getCombination(arr, n) {
+    if(n===1) return arr.map((v)=> [v]);
+    const results = [];
+    
+    arr.forEach((fixed, idx, origin) =>{
+        const rest = origin.slice(idx + 1);
+        const combinations = getCombination(rest, n-1);
+        const attached = combinations.map((v) => [fixed, ...v]);
+        results.push(...attached);
+    })
+    
+    return results
 }
 
-function solution(number) {
- return combinations(number, 3)
-        .filter(item=> (item.reduce((acc,cur)=> acc + cur,0) === 0)).length
+function solution(numbers){
+    return getCombination(numbers, 3)
+        .filter(item=>item.reduce((acc,cur)=> acc+cur,0)===0).length
 }
