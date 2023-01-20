127d6cb7be4c7edff2752900d7fcfb28e02a7474
Sangwon Park <wkqkel@naver.com>
2023-01-21 01:30:00 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,9 @@
 function solution(phone_number) {
-   return [...phone_number].map((v,i,{length})=> i>=length-4 ? v : '*').join("")
+   // 1. map과 join
+   // return [...phone_number].map((v,i,{length})=> i>=length-4 ? v : '*').join("")
+  
+  // 2. repeat와 slice
+  // slice 음수 인덱스는 배열의 끝에서부터 몇번째인지를 나타냄.
+  // 두번째 인자는 default가 length
+  return '*'.repeat(phone_number.length - 4) + phone_number.slice(-4)
 }
