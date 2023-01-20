eb46ac3e4ad5a272ea135498fdfe10e740c451d5
Sangwon Park <wkqkel@naver.com>
2023-01-21 00:31:13 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,6 +1,12 @@
 function solution(s){
+  // 1.filter
   // const getFiltered = (alphabet) => [...s.toLowerCase()].filter(v => v===alphabet)
   // return  getFiltered('p').length === getFiltered('y').length
     
- return s.toLowerCase().split('p').length === s.toLowerCase().split('y').length;
+ // 2.split
+ // return s.toLowerCase().split('p').length === s.toLowerCase().split('y').length;
+    
+ // 3.regex 
+ // match는 정규식을 만족하는 것 ig는 대소문자 구분 X
+ return s.match(/p/ig)?.length === s.match(/y/ig)?.length
 }
