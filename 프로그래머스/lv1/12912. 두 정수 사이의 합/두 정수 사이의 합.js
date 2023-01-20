444be598e28994e2e152f36d8854c9660c256068
Sangwon Park <wkqkel@naver.com>
2023-01-21 01:01:21 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -2,6 +2,6 @@ function solution(a, b) {
   // [a,b] = [a,b].sort((a,b)=> a-b)
   // return Array(b-a+1).fill(a).map((v,i)=>v+i).reduce((acc,cur)=> acc+cur,0)
   
-  // 1-10까지의 합은 (1+10) * (10-1+1) / 2 = 55
+  // 가우스 수열의 합: 1-10까지의 합은 (1+10) * (10-1+1) / 2 = 55
   return (a+b) * (Math.abs(a-b)+1) / 2
 }
