89921a817676767c00fc8e01bec98dfc897c8e61
Sangwon Park <wkqkel@naver.com>
2023-01-21 02:59:26 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,7 +1,7 @@
 function solution(s) {
     // Math.max는 만약 인수 중 하나라도 숫자로 변환하지 못한다면 NaN로 반환
-    // isNaN으로 숫자인지 판별할 수 있지만,
-    // 문자열에 e가 포함되면 숫자로 인식할 수 있어, 이를 제외하는 조건이 하나 더 필요
+    // isNaN으로 숫자인지 판별 가능
+    // 문자열에 e가 포함되면 숫자로 인식할 수 있음.
     // return [4,6].includes(s.length) && !!(Math.max(...s) + 1 )
     return [4,6].includes(s.length) && s.split('').every(v=>!isNaN(v))
 }
