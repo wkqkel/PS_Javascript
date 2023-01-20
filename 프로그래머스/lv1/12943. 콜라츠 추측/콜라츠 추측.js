8bcc3a5ff06f38293d41ff0ec1578bec07212dcc
Sangwon Park <wkqkel@naver.com>
2023-01-21 01:16:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,9 +1,16 @@
-function solution(num) {
-  let count = 0;
-  const collatz = (n) => {
-     if(n===1) return count;
-     count++
-     return collatz(n % 2 ? n * 3 + 1 : n / 2)
-  }
-  return collatz(num) > 500 ? -1 : count
+// function solution(num) {
+//   let count = 0;
+//   const collatz = (n) => {
+//      if(n===1) return count;
+//      count++
+//      return collatz(n % 2 ? n * 3 + 1 : n / 2)
+//   }
+//   return collatz(num) > 500 ? -1 : count
+// }
+
+// 문제 자체를 collatz로 만들어서 바로 Return 할 수 있게끔.
+function solution(n, count = 0) {
+  return  n === 1 
+      ? (count> 500? -1: count) 
+      : solution(n % 2 ? n * 3 + 1 : n / 2, ++count)
 }
