20673a240fd4268738019bda49aea21a2ccd5e27
Sangwon Park <wkqkel@naver.com>
2023-01-21 01:46:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,5 @@
 function solution(numbers) {
- return Array(10).fill(0).map((v,i)=> v+i)
-      .filter(v=> !numbers.includes(v)).reduce((acc,cur)=> acc+cur,0)
+ // return Array(10).fill(0).map((v,i)=> v+i)
+ //      .filter(v=> !numbers.includes(v)).reduce((acc,cur)=> acc+cur,0)
+ return 45 - numbers.reduce((acc,cur)=> acc+cur,0)
 }
