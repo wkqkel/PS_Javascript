bddc3c33f024bcab3c8c60134b9465a610e73dc9
Sangwon Park <wkqkel@naver.com>
2023-01-22 21:09:32 +0900

    [level 1] Title: 예산, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -6,12 +6,7 @@ function solution(d, budget) {
    //    if(sum>budget) return i
    // }
    //  return d.length
-    
-    // let sum = 0
-    // return d.sort((a,b)=>a-b).filter(item=>{
-    //     sum+=item
-    //     if(sum<=budget) return item
-    // }).length
+
    return d.sort((a,b)=>a-b)
             .map((v,i)=> d.slice(0,i+1).reduce((acc,cur)=> acc+cur),0)
             .filter(v=> v<=budget).length
