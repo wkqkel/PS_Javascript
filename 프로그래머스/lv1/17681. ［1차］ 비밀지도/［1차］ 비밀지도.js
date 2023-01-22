6adc184db96263107014546e9257e1d2bdb39131
Sangwon Park <wkqkel@naver.com>
2023-01-23 00:03:15 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,6 @@
 function solution(n, arr1, arr2) {
   function makeBinaryArr(arr) {
-     return arr.map((v,i)=> {
-         const _v = v.toString(2);
-         return '0'.repeat(n - _v.length) + _v
-    })  
+    return arr.map((v,i)=> '0'.repeat(n - v.toString(2).length) + v.toString(2))
   }
   [arr1, arr2] =  [makeBinaryArr(arr1),makeBinaryArr(arr2)]
   const arr = arr1.map((v,i)=> [...v].map((v,j)=> +v || +arr2[i][j]).join(''))
