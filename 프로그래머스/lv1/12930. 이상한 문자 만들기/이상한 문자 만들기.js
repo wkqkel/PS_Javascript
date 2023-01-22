f6a5dfb5ac3c358d488d4e84669ebf35045108c4
Sangwon Park <wkqkel@naver.com>
2023-01-22 20:16:58 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,5 +1,3 @@
 function solution(s) {
-  return s
-           .split(' ')
-           .map((item)=> [...item].map((v,i)=> i%2 ? v.toLowerCase(): v.toUpperCase()).join('')).join(' ')
+  return s.split(' ').map((item)=> [...item].map((v,i)=> i%2 ? v.toLowerCase(): v.toUpperCase()).join('')).join(' ')
 }
