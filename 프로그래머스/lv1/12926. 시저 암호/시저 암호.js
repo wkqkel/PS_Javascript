c9cf954d2d290921975d98e8a627da442afdebbb
Sangwon Park <wkqkel@naver.com>
2023-01-22 22:43:09 +0900

    [level 1] Title: 시저 암호, Time: 0.94 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,12 +1,16 @@
 
 function solution(s, n) {
-  function pushBack(v, n){
-     if(v===' ') return ' '
-     const idx = (alphabet.indexOf(v.toLowerCase()) + n) % alphabet.length
-     if(v===v.toUpperCase()) return alphabet[idx].toUpperCase()
-     return alphabet[idx]
-  }
+//   function pushBack(v, n){
+//      if(v===' ') return ' '
+//      const idx = (alphabet.indexOf(v.toLowerCase()) + n) % alphabet.length
+//      if(v===v.toUpperCase()) return alphabet[idx].toUpperCase()
+//      return alphabet[idx]
+//   }
 
-   return [...s].map(v=> pushBack(v, n)).join('')
+//    return [...s].map(v=> pushBack(v, n)).join('')
+    
+    const alphabets = alphabet + alphabet;
+    const clever = alphabets + alphabets.toUpperCase() + '                                        ';
+    return [...s].map(v=> clever[clever.indexOf(v) + n]).join('')
 }
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
