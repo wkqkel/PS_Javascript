23c17c19b4b80be09b53ad3541cbd15b880ce750
Sangwon Park <wkqkel@naver.com>
2023-01-22 21:42:15 +0900

    [level 1] Title: 최소직사각형, Time: 5.95 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=javascript) 
 
 ### 성능 요약
 
-메모리: 41.2 MB, 시간: 10.38 ms
+메모리: 42.1 MB, 시간: 5.95 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,4 +1,13 @@
 function solution(sizes) {
-    const sorted = sizes.map(([w,h]) => [w,h].sort((a,b)=> a-b))
-    return Math.max(...sorted.map(([w])=>w)) * Math.max(...sorted.map(([_,h])=>h))
+//     const sorted = sizes.map(([w,h]) => [w,h].sort((a,b)=> a-b))
+//     return Math.max(...sorted.map(v=>v[0])) * Math.max(...sorted.map(v=>v[1]))
+    
+   let maxWidth = 0;
+   let maxHeight = 0;
+   sizes.forEach(size => {
+        let [w,h] = size.sort((a,b)=>b-a)
+        if(w > maxWidth) maxWidth = w;
+        if(h > maxHeight) maxHeight = h;
+    })
+   return maxWidth * maxHeight
 }
