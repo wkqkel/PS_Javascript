4c8689902313e97e6b20f4f112061f1a907eec18
Sangwon Park <wkqkel@naver.com>
2023-01-22 19:26:13 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,18 +1,25 @@
 
-
-function solution(a, b) {
-  let gcd, lcm
-  for(let i=Math.min(a,b); i>=1; i--){
-     if(a%i===0 && b%i===0 ) {
-         if(!gcd) gcd = i
-         if(!lcm) lcm = a * b / i
-     }
+// 방법 1. for 문으로 풀기
+// function solution(n, m) {
+//   let gcd, lcm
+//   for(let i=Math.min(n,m); i>=1; i--){
+//      if(n%i===0 && m%i===0) {
+//          if(!gcd) gcd = i
+//          if(!lcm) lcm = n * m / i
+//      }
    
-     if(lcm && gcd) return [gcd, lcm]
-  }
- 
-}
+//      if(lcm && gcd) return [gcd, lcm]
+//   }
+// }
 
+// 방법2. 재귀 이용
+function solution(n, m) {
+    return [getGreatestCommonDivision(n,m), getLeastCommonMultiple(n,m)]
+}
 function getGreatestCommonDivision(a,b){
     return b % a ? getGreatestCommonDivision(b % a ,a) : a
+}
+
+function getLeastCommonMultiple(a,b){
+     return Math.min(a,b) / getGreatestCommonDivision(a,b) * Math.max(a,b)
 }
