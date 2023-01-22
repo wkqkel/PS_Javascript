0ad08299650ebd02c172029aea785688a8d653f4
Sangwon Park <wkqkel@naver.com>
2023-01-22 19:26:38 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,16 +1,16 @@
 
 // 방법 1. for 문으로 풀기
-// function solution(n, m) {
-//   let gcd, lcm
-//   for(let i=Math.min(n,m); i>=1; i--){
-//      if(n%i===0 && m%i===0) {
-//          if(!gcd) gcd = i
-//          if(!lcm) lcm = n * m / i
-//      }
+function solution(n, m) {
+  let gcd, lcm
+  for(let i=Math.min(n,m); i>=1; i--){
+     if(n%i===0 && m%i===0) {
+         if(!gcd) gcd = i
+         if(!lcm) lcm = n * m / i
+     }
    
-//      if(lcm && gcd) return [gcd, lcm]
-//   }
-// }
+     if(lcm && gcd) return [gcd, lcm]
+  }
+}
 
 // 방법2. 재귀 이용
 function solution(n, m) {
