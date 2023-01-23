b53a8b4cebd9086d1067646e8e60e312bfcd3e67
Sangwon Park <wkqkel@naver.com>
2023-01-24 00:12:57 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,7 +1,6 @@
 function solution(s) {
-    return +s.replace(regex, (v)=> MAP.indexOf(v))
-   
+    return +s.replace(new RegExp(MAP.join('|'), 'g'), (v)=> MAP.indexOf(v))
 }
-const regex = /zero|one|two|three|four|five|six|seven|eight|nine/g
+// const regex = /zero|one|two|three|four|five|six|seven|eight|nine/g
 const MAP = ['zero', 'one', 'two','three','four',
              'five','six','seven','eight','nine']
