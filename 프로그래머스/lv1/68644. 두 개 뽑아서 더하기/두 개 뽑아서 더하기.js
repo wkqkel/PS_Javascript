1bf490160e4c5786f8b1bbae6ac517616be604c6
Sangwon Park <wkqkel@naver.com>
2023-01-24 05:12:48 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -11,5 +11,14 @@ const getCombinations = (arr, n) =>{
 }
 
 function solution(numbers) {
-    return [...new Set(getCombinations(numbers, 2).map((v)=> v[0] + v[1]).sort((a,b)=> a-b))]
+    // return [...new Set(getCombinations(numbers, 2).map((v)=> v[0] + v[1]).sort((a,b)=> a-b))]
+
+    // 2중for문으로 풀기
+    const set = new Set()
+    for(let i = 0; i<numbers.length; i++){
+        for(let j = i + 1; j<numbers.length; j++){
+           set.add(numbers[i] + numbers[j]) 
+      }
+    }
+    return [...set].sort((a,b)=> a-b)
 }
