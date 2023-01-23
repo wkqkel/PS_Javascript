33e7395ddce88a391552a5715f32ef1fdb14a5f6
Sangwon Park <wkqkel@naver.com>
2023-01-24 04:50:49 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 8.40 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,3 +1,8 @@
 function solution(strings, n) {
+    // localeCompare 
+    // referenceStr.localeCompare(compareString[, locales[, options]])
+    // referenceStr이 compareString보다 앞에 있으면 -1, 뒤에 있으면 1, 같으면 0 반환
+    // 'a'.localeCompare('b') // -1 , 
+    // sort((s1,s2)=> s1.localeCompare(s2)) === sort()
     return strings.sort((s1, s2)=> s1[n]=== s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n]))
 }
