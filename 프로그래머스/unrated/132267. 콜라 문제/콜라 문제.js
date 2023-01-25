78551d151d1d93654dc60c50410146d51c453dbf
Sangwon Park <wkqkel@naver.com>
2023-01-25 23:14:44 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,21 +1,13 @@
 function solution(a, b, n) {
-    // a병을 갖다주면 b병 주는 마트. n병 갖다준다면 몇 병을 받나?
-    // if(a < b) return 0;
-    // let left = n;
-    // let ans = 0
-    // while(~~(left/a)) {
-    //     let give = ~~(left / a) * a;
-    //     let take = ~~(give / a) * b
-    //     left = left - give + take;
-    //     ans += take;
-    // }
-    // return ans;
-
-    // Math.floor((n - b) / (a - b)) * b : n병 갖다주고 받은 병 갯수
-    // Math.floor((n - b) / (a - b)) : a병을 몇 번 갖다줬는지
-    // a-b : 한번에 갖다주면서 손해보는 양
-    // n-b => a병을 몇 번 갖다줬는지인데 
-    // 한 번 갖다줘도 b만큼 더해지기 때문에(a-b)
-    // 초기값 b를 빼줘야 a번 갖다준 양이 나옴.
-    return Math.floor((n - b) / (a - b)) * b
+   if(n<2) return 0;
+   let left = n
+   let take, give
+   let ans = 0;
+   while(left >= a){
+       take = ~~(left / a) * b
+       give = take * a / b;
+       left = left - give + take
+       ans += take
+   }
+   return ans
 }
