c95899ed4637b6a3635f78e8457379cc1449ed68
Sangwon Park <wkqkel@naver.com>
2023-01-26 22:28:57 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 17.83 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,4 +1,21 @@
 function solution(gems) {
-    var answer = [];
-    return answer;
+    const set = new Set(gems)
+    const map = new Map([[gems[0], 1]])
+    let p1 = p2 = 0;
+    let ans = [0, gems.length]
+    
+    while(p2 < gems.length){
+        if(map.size === set.size && p2 - p1 < ans[1] - ans[0]) {
+            ans = [p1+1,p2+1]
+        }
+        if(map.size < set.size){
+            p2++
+            map.set(gems[p2], ~~map.get(gems[p2]) + 1)
+        } else {
+            map.set(gems[p1], map.get(gems[p1]) - 1)
+            if(!map.get(gems[p1])) map.delete(gems[p1])
+            p1++
+        }
+    }
+    return ans
 }
