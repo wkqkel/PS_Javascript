fd51488d4b375a54fd633b1c540f1da223f4299e
Sangwon Park <wkqkel@naver.com>
2023-01-27 20:27:52 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,5 +1,16 @@
 function solution(N, stages) {
-    const arr = Array(N).fill(1).map((v,i)=>v+i);
-
-     return arr.map((n)=> [stages.filter(v=> v===n).length / stages.filter(v=>v>=n).length ,n]).sort((a,b)=> b[0]-a[0]).map(v=>v[1])
+    // const arr = Array(N).fill(1).map((v,i)=>v+i);
+    //  return arr.map((n)=> {
+    //      const failure = stages.filter(v=> v===n).length 
+    //                      / stages.filter(v=>v>=n).length
+    //      return [failure, n]
+    //  }).sort((a,b)=> b[0]-a[0]).map(v=>v[1])
+    
+    const arr = []
+    for(let i=1; i<=N; i++){
+         const failure = stages.filter(v=> v===i).length 
+                         / stages.filter(v=>v>=i).length
+         arr.push([failure, i])
+    }
+    return arr.sort((a,b)=> b[0]-a[0]).map(v=>v[1])
 }
