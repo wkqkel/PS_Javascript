4028bf79d077070d01e388d5eef93f793549e228
Sangwon Park <wkqkel@naver.com>
2023-01-27 23:01:28 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,13 +1,19 @@
 function solution(lottos, win_nums) {
-   const arr = [];
-   const bonus = lottos.filter(v=>!v).length
-   for(let x of lottos){
-       if(win_nums.includes(x)){
-           arr.push(x)
-       }
-   }
-   const max = arr.length + bonus
-   const min = arr.length
-   const rankMap = [6, 6, 5, 4, 3, 2, 1]
-   return [rankMap[max], rankMap[min]]
+   // const arr = [];
+   // const bonus = lottos.filter(v=>!v).length
+   // for(let x of lottos){
+   //     if(win_nums.includes(x)){
+   //         arr.push(x)
+   //     }
+   // }
+   // const max = arr.length + bonus
+   // const min = arr.length
+   // const rankMap = [6, 6, 5, 4, 3, 2, 1]
+   // return [rankMap[max], rankMap[min]]
+    
+    const minCount = lottos.filter(v=> win_nums.includes(v)).length;
+    const zeroCount = lottos.filter(v=>!v).length;
+    const maxCount = minCount + zeroCount;
+    const rankMap = [6, 6, 5, 4, 3, 2, 1]
+    return [rankMap[maxCount], rankMap[minCount]]
 }
