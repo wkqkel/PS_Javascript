510bebdf75227c0c92c65c7c28ec674483dfb368
Sangwon Park <wkqkel@naver.com>
2023-01-28 15:37:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,5 +1,5 @@
 function solution(n, arr1, arr2) {
-  // 방법1
+  // 방법1 0.23ms
   // function makeBinaryArr(arr) {
   //   return arr.map((v,i)=> '0'.repeat(n - v.toString(2).length) + v.toString(2))
   // }
@@ -7,7 +7,7 @@ function solution(n, arr1, arr2) {
   // return arr1.map((v,i)=> [...v].map((v,j)=> MAP[+v || +arr2[i][j]]).join(''))
 
 
-  // 방법2
+  // 방법2 0.17ms
   // function addZero(n, s) { return '0'.repeat(n - s.length) + s; }
   // return arr1.map((v,i)=> addZero(n,(v | arr2[i]).toString(2))
   //                 .replace(/1|0/g, v=>MAP[v]))
@@ -19,8 +19,9 @@ function solution(n, arr1, arr2) {
       const res = arr1[i] | arr2[i];
       const bit = res.toString(2);
       const blankStr = " ".repeat(n - bit.length)
-      // const bitStr = bit.replaceAll('1', '#').replaceAll('0', ' ')
-      const bitStr = bit.replace(/1/gi, '#').replace(/0/gi, ' ') 
+     
+      // const bitStr = bit.replaceAll('1', '#').replaceAll('0', ' ')  // 0.6ms
+      const bitStr = bit.replace(/1/gi, '#').replace(/0/gi, ' ') // 0.23ms
       ans.push(blankStr + bitStr)
   }
   return ans
