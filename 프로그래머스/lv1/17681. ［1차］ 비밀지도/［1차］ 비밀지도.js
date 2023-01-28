52f83196fab0506b545801a0effee702a7153419
Sangwon Park <wkqkel@naver.com>
2023-01-28 15:33:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,15 +1,28 @@
 function solution(n, arr1, arr2) {
+  // 방법1
   // function makeBinaryArr(arr) {
   //   return arr.map((v,i)=> '0'.repeat(n - v.toString(2).length) + v.toString(2))
   // }
   // [arr1, arr2] =  [makeBinaryArr(arr1),makeBinaryArr(arr2)]
-  // const arr = arr1.map((v,i)=> [...v].map((v,j)=> +v || +arr2[i][j]).join(''))
-  // return  arr.map((v)=> [...v].map((v)=> MAP[v]).join(''))
+  // return arr1.map((v,i)=> [...v].map((v,j)=> MAP[+v || +arr2[i][j]]).join(''))
 
 
-  function addZero(n, s) { return '0'.repeat(n - s.length) + s; }
-  return arr1.map((v,i)=> addZero(n,(v | arr2[i]).toString(2))
-                  .replace(/1|0/g, v=>MAP[v]))
+  // 방법2
+  // function addZero(n, s) { return '0'.repeat(n - s.length) + s; }
+  // return arr1.map((v,i)=> addZero(n,(v | arr2[i]).toString(2))
+  //                 .replace(/1|0/g, v=>MAP[v]))
+    
+    
+  // 방법3
+  const ans = [];
+  for(let i = 0; i < n; i++){
+      const res = arr1[i] | arr2[i];
+      const bit = res.toString(2);
+      const blankStr = " ".repeat(n - bit.length)
+      const bitStr = (blankStr + bit).replaceAll('1', '#').replaceAll('0', ' ')
+      ans.push(bitStr)
+  }
+  return ans
 }           
 
 const MAP = [' ', '#']
