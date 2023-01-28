983e3c220f70247a8abecebcfdf8aaa918b5ccd3
Sangwon Park <wkqkel@naver.com>
2023-01-29 01:09:32 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,5 +1,22 @@
 function solution(dartResult) {
-    const POW_MAP = {S:1, D:2,T:3}
-   return dartResult.split(v=> /^d/)
-
+   const bonus = ['_', 'S', 'D', 'T']
+   const points = []
+   let temp = ''
+   for(x of dartResult) {
+       // x가 숫자이면 temp에 더해줌
+      if(!isNaN(x)) temp += x
+       // x가 숫자가 아니면 이전 값 계산
+      if(bonus.includes(x)) {
+         points.push(Math.pow(+temp, bonus.indexOf(x)))
+         temp = ''
+      }
+      if(x=== '#') {
+          points[points.length -1] *= -1;
+      }
+      if(x=== '*') {
+          points[points.length -1] *= 2;
+          points[points.length -2] *= 2;
+      }
+   }
+   return points.reduce((acc,cur)=> acc+cur, 0)
 }
