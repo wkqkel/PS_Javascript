010c267fa03f2ebb63b4eceb8146d2572c35a6b6
Sangwon Park <wkqkel@naver.com>
2023-01-31 01:57:22 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,25 +1,39 @@
 function solution(X, Y) {
-  // 두개의 교집합을 만들자 없으면 리턴 -1하자
-    // 교집합 어떻게? ㅎ
-  const arr = [];
-  const yMap = new Map();
-
-  for (let y of Y) {
-      yMap.set(y, (yMap.get(y) || 0) + 1);
-  }
+    const arr = [];
+    const yMap = {};
     
-  for (let x of X) {
-      if(yMap.has(x)){ 
-          arr.push(x);
-          yMap.set(x, yMap.get(x) - 1);
-          if(yMap.get(x) === 0) yMap.delete(x);
-      }
-  }
+    for (let y of Y) {
+      yMap[y] = (yMap[y] || 0) + 1;
+    }
     
-  if(arr.length === 0) return '-1'
-  if(arr[0] === '0') return '0';
-  return arr.sort().reverse().join('')
-  
+    for (let x of X) {
+      if(!yMap[x]) continue;
+      arr.push(x);
+      yMap[x]--
+    }
+    
+    if(arr.length === 0) return '-1'
+    if(arr[0] === '0') return '0';
+    return arr.sort().reverse().join('')   
+    
+//   // 두개의 교집합을 만들자 없으면 리턴 -1하자
+//     // 교집합 어떻게? ㅎ
+//   const arr = [];
+//   const yMap = new Map();
 
-
+//   for (let y of Y) {
+//       yMap.set(y, (yMap.get(y) || 0) + 1);
+//   }
+    
+//   for (let x of X) {
+//       if(yMap.has(x)){ 
+//           arr.push(x);
+//           yMap.set(x, yMap.get(x) - 1);
+//           if(yMap.get(x) === 0) yMap.delete(x);
+//       }
+//   }
+    
+//   if(arr.length === 0) return '-1'
+//   if(arr[0] === '0') return '0';
+//   return arr.sort().reverse().join('')
 }
