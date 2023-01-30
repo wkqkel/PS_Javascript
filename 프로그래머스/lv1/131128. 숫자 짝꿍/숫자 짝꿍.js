e359a2be11f957d14eb959a337cb75bc6cdb02e2
Sangwon Park <wkqkel@naver.com>
2023-01-31 01:52:20 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,24 @@
+function solution(X, Y) {
+  // 두개의 교집합을 만들자 없으면 리턴 -1하자
+    // 교집합 어떻게? ㅎ
+  const arr = [];
+  const yMap = new Map();
+  // x가 더 length 가 적다면?
+  if(X.length > Y.length) [X,Y] = [Y,X];
+  for (let y of Y) {
+      yMap.set(y, (yMap.get(y) || 0) + 1);
+  }
+  for (let x of X) {
+      if(yMap.has(x)){ 
+          arr.push(x);
+          yMap.set(x, yMap.get(x) - 1);
+          if(yMap.get(x) === 0) yMap.delete(x);
+      }
+  }
+  if(arr.length === 0) return '-1'
+  if(arr[0] === '0') return '0';
+  return arr.sort().reverse().join('')
+  
+
+
+}
