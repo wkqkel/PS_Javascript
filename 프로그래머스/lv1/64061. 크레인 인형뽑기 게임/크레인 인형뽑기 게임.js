84c45f46aa64549bd36c68b97c3ad4fac734c4da
Sangwon Park <wkqkel@naver.com>
2023-02-01 00:57:03 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.52 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,20 +1,41 @@
 function solution(board, moves) {
     // 먼저 들어온게 늦게 나감 stack
+    // const stack = [];
+    // let count = 0
+    // for(let move of moves) {
+    //    for(let j = 0; j < board.length; j++){
+    //        if(!board[j][move-1]) continue;
+    //        stack.push(board[j][move-1])
+    //        board[j][move-1] = 0;
+    //        if(stack[stack.length - 1] === stack[stack.length -2]) {
+    //            count += 2
+    //            stack.splice(-2,2)
+    //        }
+    //        break;
+    //    }
+    // }
+    // return count;
+    
+    // 인프런 강의 풀이
     const stack = [];
-    let count = 0
-    for(let move of moves) {
-       for(let j = 0; j < board.length; j++){
-           if(!board[j][move-1]) continue;
-           stack.push(board[j][move-1])
-           board[j][move-1] = 0;
-           if(stack[stack.length - 1] === stack[stack.length -2]) {
-               count += 2
-               stack.splice(-2,2)
-           }
-           break;
-       }
-    }
-    return count;
+    let ans = 0;
+    
+    moves.forEach(pos =>{
+        for(let i=0; i<board.length; i++){
+            if(board[i][pos-1]!==0){
+                let tmp = board[i][pos-1];
+                board[i][pos-1] = 0;
+                if(tmp === stack[stack.length - 1]){
+                    stack.pop();
+                    ans += 2;
+                } else {
+                    stack.push(tmp)
+                }
+                break;
+            }
+        }
+    });
+    return ans
 }
 
 // [[0,0,0,0,0],
