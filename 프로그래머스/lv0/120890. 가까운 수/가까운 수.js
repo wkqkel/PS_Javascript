7167e7f2221a531e641da4c3fb3047d50f52bae4
Sangwon Park <wkqkel@naver.com>
2023-02-01 23:49:56 +0900

    [level 0] Title: 가까운 수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,6 +1,9 @@
 function solution(array, n) {
-   const sorted = [...array].sort((a,b)=> a-b);
-   const minDiffArray = sorted.map(((item)=> Math.abs(n - item)))
-   const minDiff = Math.min(...minDiffArray)
-   return sorted[minDiffArray.indexOf(minDiff)];
+   const sorted = array.sort((a,b)=> a-b).map(((item)=> Math.abs(n - item)))
+   return array[sorted.indexOf(Math.min(...sorted))];
+    
+   // const sorted = [...array].sort((a,b)=> a-b);
+   // const minDiffArray = array.map(((item)=> Math.abs(n - item)))
+   // const minDiff = Math.min(...minDiffArray)
+   // return sorted[minDiffArray.indexOf(minDiff)];
 }
