ca58e1b4df72d95bc168d4560c27cc3f7512bc28
Sangwon Park <wkqkel@naver.com>
2023-02-02 00:38:23 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.01 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,44 +1,39 @@
 const returnPos = (x) => {
-    const pos = []
-    pos[0] = keypad.findIndex(v=> v.includes(x))
-    pos[1] = keypad[pos[0]].indexOf(x)
-    return pos
+    const pos = [];
+    pos[0] = keypad.findIndex(v=> v.includes(x));
+    pos[1] = keypad[pos[0]].indexOf(x);
+    return pos;
 }
 
 const getPosDiff = (pos1, pos2) => {
-   return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1])
+   return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
 }
 
 function solution(numbers, hand) {
-    const MAP = {1:'L',4:'L',7:'L', 3:'R', 6:'R', 9:'R'}
-    const handsPos = ['*', '#']
-    let res = ''
+    const MAP = {1:'L',4:'L',7:'L', 3:'R', 6:'R', 9:'R'};
+    const handsPos = ['*', '#'];
+    let res = '';
     for(let x of numbers){
         if(MAP[x]) {
-           handsPos[+(MAP[x] === 'R')] = x
-           res += MAP[x]
+           handsPos[+(MAP[x] === 'R')] = x;
+           res += MAP[x];
         } else{
             // 2, 5, 8, 0
-           const goalPos = returnPos(x)
-           const leftPos = returnPos(handsPos[0])
-           const rightPos = returnPos(handsPos[1])
-           const leftDiff = getPosDiff(leftPos, goalPos)
-           const rightDiff = getPosDiff(rightPos, goalPos)
-           if(leftDiff < rightDiff) {
-                handsPos[0] = x
-                res += 'L'
-           }
-           if(leftDiff > rightDiff) {
-               handsPos[1] = x
-               res += 'R'
-           }
+           const goalPos = returnPos(x);
+           const leftPos = returnPos(handsPos[0]);
+           const rightPos = returnPos(handsPos[1]);
+           const leftDiff = getPosDiff(leftPos, goalPos);
+           const rightDiff = getPosDiff(rightPos, goalPos);
            if(leftDiff === rightDiff) {
-               handsPos[+(hand === 'right')] = x
-               res += hand === 'right' ? 'R' : "L"
+               handsPos[+(hand === 'right')] = x;
+               res += hand === 'right' ? 'R' : "L";
+           } else {
+               handsPos[+(leftDiff > rightDiff)] = x;
+               res += leftDiff > rightDiff ? 'R' : 'L';
            }
         }
-     
     }
-   return res
+   return res;
 }
+
 const keypad = ['123', '456', '789', '*0#'];
