67b1df2b8da13e45a8ba728fa5eb8becd27cdf51
Sangwon Park <wkqkel@naver.com>
2023-02-03 03:24:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,11 @@
+function solution(survey, choices) {
+    // 첫번쨰원소는  3~1 0 1~3
+
+    const MAP = {1:3, 2:2,3:1, 4: 0 ,5:1,6:2,7:3}
+    const obj = {}
+    const PERSONALITY_TYPES = ['RT','CF', 'JM', 'AN']
+    for(let i = 0; i< survey.length; i++) {
+        obj[survey[i][+(choices[i]-4 > 0)]] = (obj[survey[i][+(choices[i]-4 > 0)]] || 0) + MAP[choices[i]]
+    }
+    return PERSONALITY_TYPES.map(v => ~~obj[v[0]] >= ~~obj[v[1]] ? v[0]: v[1]).join('')
+}
