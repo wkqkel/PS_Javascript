66247348e728f85fb7208a10bf823bbd2185d089
Sangwon Park <wkqkel@naver.com>
2023-02-05 05:35:04 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -2,6 +2,7 @@ function solution(s) {
     // const arr = s.split(' ').sort((a,b)=> +a - +b);
     // return `${arr[0]} ${arr[arr.length - 1]}`
     
+    // 아래가 시간 더 적게 걸림
     const arr = s.split(' ');
     return `${Math.min(...arr)} ${Math.max(...arr)}`
 }
