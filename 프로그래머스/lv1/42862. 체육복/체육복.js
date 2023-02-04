3fe930ca2fab51facb7e331dd95b9deab0c95f30
Sangwon Park <wkqkel@naver.com>
2023-02-05 04:54:11 +0900

    [level 1] Title: 체육복, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,15 +1,13 @@
 function solution(n, lost, reserve) {
   const arr = Array(n).fill(true).map((v,i)=> !lost.includes(i+1) || reserve.includes(i+1));
-  const canArr = Array(n).fill(true).map((v,i)=> reserve.includes(i+1) && !lost.includes(i+1))
-
+  const canArr = Array(n).fill(true).map((v,i)=> reserve.includes(i+1) && !lost.includes(i+1));
+  // if 기준을 arr이 없으므로 줄 수 있는 애한테 받는 식으로 해야함.
   for(let i=0; i<n; i++){
-      if(!arr[i]) {
-          if(canArr[i-1] || canArr[i+1]) {
-              const idx = canArr[i-1] ? i - 1 : i + 1;
-              canArr[idx] = false;
-              arr[i] = true;
-          }
+      if(!arr[i] && (canArr[i-1] || canArr[i+1])) {
+          const idx = canArr[i-1] ? i - 1 : i + 1;
+          canArr[idx] = false;
+          arr[i] = true;
       }
   }
-  return arr.filter(Boolean).length
+  return arr.filter(Boolean).length;
 }
