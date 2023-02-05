009bd98c8b75f55d7f09894bafcc5dfeff3c1dde
Sangwon Park <wkqkel@naver.com>
2023-02-05 23:59:30 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,4 +1,4 @@
 function solution(s) {
-    return s.toLowerCase().replace(/(^| )\w/g, (v)=> v.toUpperCase())
-   
+    return s.split(' ').map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(' ');
+    return s.toLowerCase().replace(/(^| )\w/g, (v)=> v.toUpperCase());
 }
