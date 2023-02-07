f69ea5eff2f158b1e14e38256c5ea47f26c89d33
Sangwon Park <wkqkel@naver.com>
2023-02-07 21:56:05 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.82 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+   function toBinary(before, ans){
+       if(before === '1') return ans;
+       ans[0]++
+       return toBinary(before.replaceAll('0',()=>{ans[1]++; return ''}).length.toString(2), ans)
+   }
+    return toBinary(s, [0,0]);
+}
