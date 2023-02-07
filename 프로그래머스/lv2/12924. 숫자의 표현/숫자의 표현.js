c4b0599d3637f2d5190b9c348c572fbeb2d8949a
Sangwon Park <wkqkel@naver.com>
2023-02-07 22:42:22 +0900

    [level 2] Title: 숫자의 표현, Time: 4.77 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,24 @@
+function solution(n) {
+    if([1,2].includes(n)) return 1;
+    
+    const arr = Array(~~(n/2)+1).fill(1).map((v,i)=> v + i)
+    let count = left = right = 0;
+    while(right !== arr.length) {
+       const sum = arr.slice(left, right+1).reduce((acc,cur)=> acc + +cur,0);
+       
+       if(sum === n) count++
+       if(sum <= n) right++
+       if(sum >= n) left++
+    }
+    
+    return count + 1;
+}
+
+// 15보다 클때까지 뒤에 더해
+// 중간에 같으면 count++ 
+// 15넘어가면 작을때까지 빼
+// 중간에 같으면 
+// 15보다 작아지면, 다시 뒤에꺼더해
+// 그리고 right가 끝에 다달고 left가 
+ 
+// 절반까지만 하면됨.
