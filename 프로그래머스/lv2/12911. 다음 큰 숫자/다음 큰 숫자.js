54dd17a7d744df56328cff3664754c7538f618c6
Sangwon Park <wkqkel@naver.com>
2023-02-09 00:43:21 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,9 +1,14 @@
 function solution(n) {
-    let next = n + 1; 
-    const returnN1Binary = (n) => n.toString(2).replaceAll('0','').length
-    const nN1Ninary = returnN1Binary(n)
-    while(true) {
-     if(nN1Ninary  === returnN1Binary(next)) return next;
-     next++;
+    // let next = n + 1; 
+    // const returnN1Binary = (n) => n.toString(2).replaceAll('0','').length
+    // const nN1Ninary = returnN1Binary(n)
+    // while(true) {
+    //  if(nN1Ninary  === returnN1Binary(next)) return next;
+    //  next++;
+    // }
+    
+    const size = n.toString(2).match(/1/g).length;
+    while(n++){
+        if(size===n.toString(2).match(/1/g).length) return n
     }
 }
