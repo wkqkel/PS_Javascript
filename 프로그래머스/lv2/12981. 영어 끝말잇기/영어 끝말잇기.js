d3ecd780fe6acc8682e2dbbe4861847e17584e63
Sangwon Park <wkqkel@naver.com>
2023-02-10 20:47:08 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,13 +1,17 @@
-function solution(n, words) {
- const isFail = (words, i) => {
+const isFail = (words, i) => {
     if(i===0) return false;
-    if(words[i-1][words[i-1].length -1]!==words[i][0]) return true;
-    if(words.slice(0,i).includes(words[i])) return true;
-    if(words[i].length === 1) return true;
-    return false;
+    const [prev, curr] = [words[i-1], words[i]]
+    return (prev[prev.length -1]!==curr[0] ||
+            words.slice(0,i).includes(curr) ||
+            curr.length === 1)
  }
- for(let i=0; i<words.length; i++){
-     if(isFail(words, i)) return [(i+1)%n|| n,Math.ceil((i+1) / n)]
- }
- return [0,0]
+     
+  
+function solution(n, words) {
+   
+     for(let i=0; i<words.length; i++){
+         if(isFail(words, i)) return [(i+1)%n || n, Math.ceil((i+1) / n)]
+     }
+    
+     return [0,0]
 }
