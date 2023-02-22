a16ca645aa11e3378851c1fc3a405949f688c600
Sangwon Park <wkqkel@naver.com>
2023-02-23 00:29:52 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,10 +1,8 @@
 
 function solution(n) {
-    let answer = 0, arr = [0, 1];
-    for (let a = 2; a <= n + 1; a++) {
-      answer = arr[0] + (arr[1] % 1234567);
-      arr[0] = arr[1];
-      arr[1] = answer;
+    const arr = [1, 1];
+    for (let i = 2; i <= n; i++) {
+      arr[i] = (arr[i-1] + arr[i-2]) % 1234567
     }
-    return answer % 1234567;
+    return arr[arr.length - 1] % 1234567
   };
