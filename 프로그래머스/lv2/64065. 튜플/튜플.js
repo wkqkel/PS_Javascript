4fb45cd77078f46815bca814c2595601d5378ef1
Sangwon Park <wkqkel@naver.com>
2023-04-01 20:05:30 +0900

    [level 2] Title: 튜플, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,25 +1,29 @@
 function solution(s) {
-    const arr = [...s].reduce((acc,cur)=> {
-        if(!isNaN(+cur)) {
-          acc[acc.length - 1] += cur
-        }
-         if(cur===','){
-            acc[acc.length] = ''
-        }
-        return acc
-    },[''])
+    // 내 방법 
+    // s를 반복문 돌면서 이중배열로 만든 다음,
+    // map객체에 값 당 갯수 담고,(처음에는 객체를 이용=>시간초과)
+    // 내림차순 정렬해서 키를 반환
+//     const arr = [...s].reduce((acc,cur)=> {
+//         if(!isNaN(+cur)) {
+//           acc[acc.length - 1] += cur
+//         }
+//          if(cur===','){
+//             acc[acc.length] = ''
+//         }
+//         return acc
+//     },[''])
     
-    const map = arr.reduce((acc, v) => {
-      acc.set(v, (acc.get(v) || 0) + 1);
-      return acc;
-    }, new Map());
+//     const map = arr.reduce((acc, v) => {
+//       acc.set(v, (acc.get(v) || 0) + 1);
+//       return acc;
+//     }, new Map());
     
-    return [...map].sort((a,b)=> b[1] - a[1]).map(v=> +v[0]);
+//     return [...map].sort((a,b)=> b[1] - a[1]).map(v=> +v[0]);
     
-    // const map = arr.reduce((acc,cur)=> ({...acc, [cur]: (acc[cur]|0) + 1}) ,{})
-    // const sortedMap = Object.entries(map).sort((a,b)=> b[1] - a[1])
-    // return sortedMap.map(v=> +v[0])
-    
-    // const map = new Map();
-    // arr.forEach((v)=> map.set(v, (map.get(v) | 0) + 1 ))
+    // 방법2
+    // JSON.parse와 replace를 이용해 만들고,
+    // 배열의 length가 적을수록 올바른 순서로 존재
+    // 정렬 후 flat해주고 set으로 중복을 없애준 것을 반환
+     return [...new Set(JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'))
+                .sort((a,b)=> a.length -b.length).flat())]
 }
