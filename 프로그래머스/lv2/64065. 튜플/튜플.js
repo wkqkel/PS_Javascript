3d01dc59cc3c08c58e60031259b97b23d9d43274
Sangwon Park <wkqkel@naver.com>
2023-04-01 19:45:19 +0900

    [level 2] Title: 튜플, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,20 +1,25 @@
 function solution(s) {
-    const arr = ['']
-    for(let x of s){
-        if(!isNaN(+x)) {
-          arr[arr.length - 1] += x
-          continue;
+    const arr = [...s].reduce((acc,cur)=> {
+        if(!isNaN(+cur)) {
+          acc[acc.length - 1] += cur
         }
-        if(x===','){
-            arr[arr.length] = ''
+         if(cur===','){
+            acc[acc.length] = ''
         }
-    }
-  
+        return acc
+    },[''])
     
-    const map = new Map();
-    arr.forEach((v)=> map.set(+v, (map.get(+v) | 0) + 1 ))
-    return [...map].sort((a,b)=> b[1] - a[1]).map(v=>+v[0])
+    const map = arr.reduce((acc, v) => {
+      acc.set(v, (acc.get(v) || 0) + 1);
+      return acc;
+    }, new Map());
+    
+    return [...map].sort((a,b)=> b[1] - a[1]).map(v=> +v[0]);
+    
     // const map = arr.reduce((acc,cur)=> ({...acc, [cur]: (acc[cur]|0) + 1}) ,{})
     // const sortedMap = Object.entries(map).sort((a,b)=> b[1] - a[1])
     // return sortedMap.map(v=> +v[0])
+    
+    // const map = new Map();
+    // arr.forEach((v)=> map.set(v, (map.get(v) | 0) + 1 ))
 }
