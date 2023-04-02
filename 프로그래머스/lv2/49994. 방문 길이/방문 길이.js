85b915f2c448004d32de7c9f618318508af5507e
Sangwon Park <wkqkel@naver.com>
2023-04-02 15:18:18 +0900

    [level 2] Title: 방문 길이, Time: 1.73 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -1,33 +1,35 @@
-const MOVE_MAP = {
-         U: [0,1],
-         D: [0,-1],
-         R: [1,0],
-         L: [-1,0],
-        }
+
 class Character {
+    moveMap = MOVE_MAP
     position = [0,0]
     history = new Set()
+
     move(direction) {
-        const add = MOVE_MAP[direction]
+        const add = this.moveMap[direction]
         if(this.checkOverPlane(this.position, add)) {
             return
         }
         const prev = [...this.position]
         this.position = [this.position[0] + add[0], 
                          this.position[1] + add[1]] 
-        // x:0,-1 y:0,0 ===  x:-1,0 y:0,0 => sorted
-        const newHistory = JSON.stringify({
-                             x:[prev[0],this.position[0]].sort(),
-                             y:[prev[1],this.position[1]].sort()
-                           })
-        this.history = this.history.add(newHistory)        
+        this.makeHistory(prev, this.position)
     }
    
     checkOverPlane(origin, add){
       const isOver = (a,b) => a+b > 5 || a+b < -5
       return isOver(origin[0],add[0]) || isOver(origin[1],add[1]);
     }
+    
+    makeHistory(prev, next) {
+        // x:0,-1 y:0,0 ===  x:-1,0 y:0,0 => sorted
+        const newHistory = JSON.stringify({
+                             x:[prev[0],next[0]].sort(),
+                             y:[prev[1],next[1]].sort()
+                           })
+        this.history = this.history.add(newHistory)  
+    }
 }
+
 function solution(dirs) {
   const character = new Character()
   for (let x of dirs){
@@ -35,3 +37,10 @@ function solution(dirs) {
   }
   return character.history.size
 }
+
+const MOVE_MAP = {
+         U: [0,1],
+         D: [0,-1],
+         R: [1,0],
+         L: [-1,0],
+        }
