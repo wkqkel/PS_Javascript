1fe847c1a089c2f05c56c143699466f1a3168c86
Sangwon Park <wkqkel@naver.com>
2023-04-02 15:12:06 +0900

    [level 2] Title: 방문 길이, Time: 1.70 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,37 @@
+const MOVE_MAP = {
+         U: [0,1],
+         D: [0,-1],
+         R: [1,0],
+         L: [-1,0],
+        }
+class Character {
+    position = [0,0]
+    history = new Set()
+    move(direction) {
+        const add = MOVE_MAP[direction]
+        const prev = [...this.position]
+        if(this.checkOverPlane(this.position, add)) {
+            return
+        }
+        this.position = [this.position[0] + add[0], 
+                         this.position[1] + add[1]] 
+        // x:0,-1 y:0,0 ===  x:-1,0 y:0,0 => sorted
+        const newHistory = JSON.stringify({
+                             x:[prev[0],this.position[0]].sort(),
+                             y:[prev[1],this.position[1]].sort()
+                           })
+        this.history = this.history.add(newHistory)        
+    }
+   
+    checkOverPlane(origin, add){
+      const isOver = (a,b) => a+b > 5 || a+b < -5
+      return isOver(origin[0],add[0]) || isOver(origin[1],add[1]);
+    }
+}
+function solution(dirs) {
+  const character = new Character()
+  for (let x of dirs){
+      character.move(x)
+  }
+  return character.history.size
+}
