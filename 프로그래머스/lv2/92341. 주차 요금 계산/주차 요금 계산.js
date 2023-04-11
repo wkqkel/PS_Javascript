29e1f173d989401f594d0fc08ffdf0202a38fe3a
Sangwon Park <wkqkel@naver.com>
2023-04-12 04:03:20 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,24 +1,25 @@
 function solution(fees, records) {
-    const 누적시간당주차요금계산 = 주차요금계산(fees) 
+    const 누적시간당주차요금계산하기 = 주차요금계산하기(fees);
     
     const 차량별시간기록 = records.reduce((acc,cur,idx)=>{
         const [시각,차량번호,내역] = cur.split(' ');
         acc.set(차량번호, [...(acc.get(차량번호)||[]),시각]) ;
         return acc;
     }, new Map());
-   const sorted차량별시간기록 = [...차량별시간기록].sort((a,b)=> a[0]-b[0]);
+   const 내림차순차량번호별시간기록 = [...차량별시간기록].sort((a,b)=> a[0]-b[0]);
     
-   const 누적주차요금 = [];
-
-   for (let [차량번호, 시간기록] of sorted차량별시간기록){
-      const 해당차량누적주차시간 = 시간기록.reduce((acc,cur,idx,list)=> idx % 2 === 0 ? acc + 시간계산(cur, list[idx+1]): acc, 0)
-      누적주차요금.push(누적시간당주차요금계산(해당차량누적주차시간))
-   }
+  const 누적주차요금 = 내림차순차량번호별시간기록.reduce((누적요금,[차량번호, 시간기록])=>{
+       const 해당차량누적주차시간 = 시간기록.reduce((acc,cur,idx,list)=> {
+                           if(idx % 2 !== 0) return acc
+                           return acc + 시간계산하기(cur, list[idx+1])
+                       }, 0);
+       return [...누적요금, 누적시간당주차요금계산하기(해당차량누적주차시간)];
+   }, []);
     
    return 누적주차요금
 }
 
-function 시간계산(입차시각, 출차시각='23:59'){
+function 시간계산하기(입차시각, 출차시각='23:59'){
    const [입차시, 입차분] = 입차시각.split(':');
    const [출차시, 출차분] = 출차시각.split(':');
 
@@ -27,10 +28,10 @@ function 시간계산(입차시각, 출차시각='23:59'){
            : (출차시 - 입차시) * 60+ (출차분 - 입차분);
 }
 
-function 주차요금계산([기본시간,기본요금,단위시간,단위요금]){
+function 주차요금계산하기([기본시간,기본요금,단위시간,단위요금]){
   return function(누적주차시간){
-    if(누적주차시간<=기본시간) return 기본요금
-    return 기본요금 + Math.ceil((누적주차시간 - 기본시간) / 단위시간 ) * 단위요금
+    if(누적주차시간<=기본시간) return 기본요금;
+    return 기본요금 + Math.ceil((누적주차시간 - 기본시간) / 단위시간 ) * 단위요금;
   }
 }
 
