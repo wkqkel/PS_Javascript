6e338538f97a3dda62c3964acafa32dff533573e
Sangwon Park <wkqkel@naver.com>
2023-04-14 10:23:04 +0900

    [Silver V] Title: K번째 수, Time: 4796 ms, Memory: 564928 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+const [N,K] = input[0].split(' ')
+const arr = input[1].split(' ').map(Number).sort((a,b)=>a - b)
+console.log(arr[K-1])
