aa006cce6383b5f891ae6fa3cfa6ecf524be0d22
Sangwon Park <wkqkel@naver.com>
2023-04-16 14:59:37 +0900

    [level 2] Title: [3차] 압축, Time: 16.46 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -8,9 +8,8 @@ class LZW {
         this.dic = ['_', ...ALPHABETS] // 사전초기화
         this.입력 = 입력 // 입력 초기화
         while(this.입력){ // 입력에서 처리하지않은 다음글자가 있다면
-             // 현재입력과 일치하는 가장 긴 문자열 w찾고 색인번호 출력
-             let 색인번호 = this.findW() 
-             const W = this.dic[색인번호]
+             // 현재입력과 일치하는 가장 긴 문자열 w찾고 색인번호와 함께 출력
+             const {W,색인번호} = this.findW() 
              this.입력 = this.입력.slice(W.length) // 입력에서 w를 제거
              const C = this.입력[0]
              const 추가단어 = W+C 
@@ -18,22 +17,21 @@ class LZW {
              this.res.push(색인번호)
         }
     }
-    findW(입력){
+    findW(){
         for(let i=this.입력.length; i>0;i--){
             const str = this.입력.slice(0,i);
             const idx = this.dic.indexOf(str);
             const isW = idx > -1 
             if(isW){
-                const 색인번호 = idx
-                return 색인번호
+                return {W:str, 색인번호:idx}
             }
         }
     }
 }
 
 function solution(msg) {
-    const aa = new LZW(msg)
-    return aa.res
+    const lzw = new LZW(msg)
+    return lzw.res
 }
 
 const ALPHABETS = Array.from({length: 26}, (v,i)=> String.fromCharCode(65+i))
