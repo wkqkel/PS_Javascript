54019254c04f8d3aece25f0d73d2ea7cad2ef86e
Sangwon Park <wkqkel@naver.com>
2023-04-24 00:40:56 +0900

    [Silver I] Title: 회의실 배정, Time: 392 ms, Memory: 43248 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-const N = input[0];
+const n = input[0];
 const arr  = [...input].slice(1).map(v=> v.split(' ').map(Number)).sort((a,b)=>{
     if(a[1] != b[1]) return a[1]-b[1]
     return a[0] - b[0]
 })
-const res = []
-for(let i=0; i<arr.length;i++){
-  if(i===0 || res.at(-1)[1]  <= arr[i][0]) {
-      res.push(arr[i])
+let cnt = 1, cur = 0;
+for(let i=1; i<n;i++){
+  if(arr[cur][1]  <= arr[i][0]) {
+      cur = i;
+      cnt++
   }
 }
-console.log(res.length)
+console.log(cnt)
