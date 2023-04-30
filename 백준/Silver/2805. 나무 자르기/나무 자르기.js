cb8fc0aec9b1914587205f2c7279c18989aa10cd
Sangwon Park <wkqkel@naver.com>
2023-05-01 02:33:37 +0900

    [Silver II] Title: 나무 자르기, Time: 1184 ms, Memory: 138016 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -1,34 +1,23 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const filePath = "/dev/stdin";
 // const input = fs.readFileSync(filePath).toString().trim().split(' '); // 1줄인경우
 const input = fs.readFileSync(filePath).toString().trim().split("\n"); // 여러줄인경우
 
-const [NM, ...TREES] = input;
-const N = NM.split(" ")[0];
-const M = NM.split(" ")[1];
-const trees = TREES[0]
-  .split(" ")
-  .map(Number)
-  .sort((a, b) => a - b);
+const [N,M] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number).sort((a,b)=>a-b);
 
-const getSlicedTreeSum = (height) => {
-  return trees.reduce((acc, cur) => acc + Math.max(cur - height, 0), 0);
-}; // NlogN
+let start = 0;
+let end = arr[arr.length - 1]
 
-let left = 0;
-let right = trees[N - 1];
-let answer = Number.MIN_SAFE_INTEGER;
-// [ 4, 26, 40, 42, 46 ]
-while (left <= right) {
-  let mid = Math.floor((left + right) / 2);
-  const sum = getSlicedTreeSum(mid);
-
-  if (sum >= M) {
-    if (mid > answer) answer = mid;
-    left = mid + 1;
+let res = 0;
+while(start <= end){
+  const mid = parseInt((start + end) / 2);
+  const sum = arr.reduce((acc,cur)=> acc + Math.max(cur-mid,0) ,0)
+  if(sum >= M) {
+    start = mid + 1;
+    res = mid
   } else {
-    right = mid - 1;
+    end = mid -1
   }
 }
-
-console.log(answer);
+console.log(res)
