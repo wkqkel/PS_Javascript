5e4a2cd73ffc1e2a21e80ce3bcfe294a704197d0
Sangwon Park <wkqkel@naver.com>
2023-05-30 03:04:11 +0900

    [level 2] Title: [1차] 캐시, Time: 1.37 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -6,17 +6,19 @@ class LRUManager {
     constructor(cacheSize){
         this.cacheSize = cacheSize
     }
-    
+
     process(city){
        const lowerCaseCity = city.toLowerCase();
        this.runtime += this.#checkIsHit(lowerCaseCity) ? 1 : 5;
        this.#caching(lowerCaseCity)
     }
-
+    // 처음엔 swap이란것을 만들어, 
+    // 존재할 경우 swap으로, 마지막으로 옮기는 방식으로 구현했는데,
+    // 이 경우 예외처리 못해주는 부분이 생기는 듯.
     #caching(city){
         const idx = this.cached.indexOf(city)
         if(idx > -1) {
-           this.cached = [...this.cached.slice(0,idx), ...this.cached.slice(idx+1)]
+           this.cached = [...this.cached.slice(0,idx), ...this.cached.slice(idx+1)];
         }
         this.cached.push(city);
         if(this.cached.length > this.cacheSize) {
@@ -27,8 +29,6 @@ class LRUManager {
     #checkIsHit(city){
         return this.cached.includes(city)
     }
-        
- 
 }
 
 function solution(cacheSize, cities) {
