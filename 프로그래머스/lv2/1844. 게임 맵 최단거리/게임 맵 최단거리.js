378eff7eabfa92c2aa9d594e58680133977c0bee
Sangwon Park <wkqkel@naver.com>
2023-06-07 03:44:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 29.18 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,28 +1,23 @@
-
-
 function solution(maps) {
-    const goal = [maps[0].length-1,maps.length-1];
-
-    const dx = [-1, 1, 0, 0]; 
-    const dy = [0, 0 ,-1, 1]; 
+    const goal = {x: maps[0].length -1, y: maps.length -1};
+    const dx = [-1,1,0,0];
+    const dy = [0,0,-1,1];
     
-    const queue = [];
-    queue.push([0,0,1])
-     
-   
-    while(queue.length){
-        const [curY,curX,move] = queue.shift();
-        if(curX===goal[0]&&curY===goal[1]) return move;
-        
-        for(let i = 0; i<4;i++){
+    const stack = [];
+    stack.push([0,0,1]);
+    
+    while(stack.length){
+        const [curX,curY,move] = stack.shift();
+        if(curX === goal.x && curY === goal.y) return move;
+        for(let i = 0; i < 4; i++){
             const nx = curX + dx[i];
             const ny = curY + dy[i];
             
-            if(nx>=0 && nx<=goal[0] && ny>=0 && ny<=goal[1] && maps[ny][nx]){
+            if(nx>=0 && nx<=goal.x && ny>=0 && ny<=goal.y && maps[ny][nx]){
                 maps[ny][nx] = 0;
-                queue.push([ny,nx,move+1])
+                stack.push([nx,ny,move+1])
             }
         }
     }
-    return -1
+    return -1;
 }
