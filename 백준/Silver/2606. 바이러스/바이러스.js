b74b75ab4278abb0ad94fe10f5fa50ba7bdb7062
Sangwon Park <wkqkel@naver.com>
2023-06-10 21:22:14 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 9512 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,30 +1,23 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(v=>v.split(' '));
+let input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 
-/** 
-/* Array(+input[0]+1 ).fill([]); // --1
-/* Array.from({ length:+input[0]+1 }, () => []));
-/* fill은 동일한 배열(객체)에 대한 참조가 모든 인덱스에 할당되어, 
-/* 한 배열을 수정하면 모든 인덱스의 배열이 변경됨.
-/* from은 콜백함수를 이용해, 각 인덱스에 대해 새로운 배열을 생성하여 독립성을 보장
-*/
-const graph = input.slice(2).sort((a,b)=> a[0] - b[0]).reduce((acc,cur)=>{
-  acc[+cur[0]].push(+cur[1]);
-  acc[+cur[1]].push(+cur[0]);
-  return acc;
-},  Array.from({ length:+input[0]+1 }, () => []));
+const n = +input[0]
+let graph = Array(n+1).fill().map(v=>[]);
 
-const visited = Array(+input[0]+1).fill(false);
-
-function dfs(graph,v,visited){
-    visited[v] = true;
-  for(i of graph[v]) {
-    if(!visited[i]) {
-      dfs(graph, i, visited)
-    }
+for (let i= 2; i<2+ +input[1]; i++) {
+  const [a,b] = input[i].split(' ').map(Number);
+  graph[a] = [...graph[a] ,b];
+  graph[b] = [...graph[b] ,a];
+}
+const visited = Array(n+1).fill(false)
+function dfs(n,graph,visited){
+  if(!visited[n]) {
+    visited[n] = true;
+    for(let x of graph[n]){
+      dfs(x,graph,visited);
+    }    
   }
 }
 
-dfs(graph, 1, visited);
-
-console.log(visited.filter(Boolean).length - 1);
+dfs(1,graph,visited)
+console.log(visited.filter(Boolean).length- 1)
