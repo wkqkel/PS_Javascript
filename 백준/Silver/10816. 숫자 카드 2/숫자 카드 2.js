640b09b19ed45d1551759a959e86da09d3b752fe
Sangwon Park <wkqkel@naver.com>
2023-06-14 07:27:04 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 720 ms, Memory: 120992 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,9 +1,8 @@
 let fs = require("fs");
-let input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const card = input[1].split(' ').reduce((acc,cur)=> {
-  acc.set(+cur, (acc.get(+cur) || 0)+1);
-  return acc;
-}, new Map())
+const map = input[1].split(' ').reduce((acc,cur)=>
+  acc.set(+cur, (acc.get(+cur) || 0) + 1)
+,new Map());
 
-console.log(input[3].split(' ').map(v=> card.get(+v)|| 0).join(' '))
+console.log(input[3].split(' ').map(v=>map.get(+v) || 0).join(' '))
