e4dd4fbf7dfc2141bc0d2c1e3fcf54227a1d87c3
Sangwon Park <wkqkel@naver.com>
2023-07-11 02:10:23 +0900

    [level 2] Title: [3차] n진수 게임, Time: 6.80 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,11 +1,13 @@
 function solution(n, t, m, p) {
-    let str = ''
-    for(let i = 0; i <= 30000; i++){
-        str += i.toString(n).toUpperCase();
+    let cnt = 0;
+    let str = '';
+    let res = '';
+    while(cnt <= t * m){
+        str += cnt.toString(n);
+        res += cnt % m === p-1 ? str[cnt] : ''
+        cnt++
     }
 
-    return str.split("").filter((v,i)=> {
-        return i % m === p-1? str[i%m]:"" 
-    }).join("").slice(0,t)
+    return res.slice(0,t).toUpperCase();
 }
     
