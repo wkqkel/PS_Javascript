1e0edfc539c552ab9c446e7c0d67f0844ea2e4bb
Sangwon Park <wkqkel@naver.com>
2023-07-17 21:23:43 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 12.13 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,3 +1,16 @@
+function solution(files) {
+   return files.sort((a,b)=> {
+       const [aHead, aNumber, aTail] = splitStringByNumber(a);
+       const [bHead, bNumber, bTail] = splitStringByNumber(b);
+       
+       if(aHead > bHead) return 1;
+       if(aHead < bHead) return -1;
+       if(aNumber > bNumber) return 1;
+       if(aNumber < bNumber) return -1;
+       return 0;
+   })
+}
+
  function splitStringByNumber(str) {
   const regex = /^(\D+)?(\d+)?(.*)$/;
   const matches = str.match(regex);
@@ -5,20 +18,4 @@
   const number = +matches[2] || '';
   const tail = matches[3] || '';
   return [ head, number, tail ];
-}
-function solution(files) {
-   return files.sort((a,b)=> {
-       const [aHead, aNumber, aTail] = splitStringByNumber(a);
-       const [bHead, bNumber, bTail] = splitStringByNumber(b);
-       
-       if(aHead !== bHead) {
-        return aHead > bHead ? 1 : -1;
-       }
-       
-       if(aNumber !== bNumber) {
-         return aNumber > bNumber ? 1 : -1;
-       }
-     
-       return 0;
-   })
 }
