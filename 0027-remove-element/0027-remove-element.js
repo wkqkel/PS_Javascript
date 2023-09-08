995254202dc4a6c1bf8dc9c33d120c7400090275
Sangwon Park <wkqkel@naver.com>
2023-09-08 22:20:45 +0900

    Time: 54 ms (54.72%), Space: 41.7 MB (80.13%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+   for(let i =0; i<nums.length; i++){
+       if(nums[i] === val) {
+           nums.splice(i, 1);
+           i--
+       }
+   }
+};
+
+removeElement([3,2,2,3], 3)
