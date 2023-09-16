abeeeab028745ef721439cc05a2674c8e648618e
Sangwon Park <wkqkel@naver.com>
2023-09-17 00:43:58 +0900

    Time: 57 ms (57.99%), Space: 43.8 MB (60.36%) - LeetHub

--- 1395-minimum-time-visiting-all-points/1395-minimum-time-visiting-all-points.js
+++ 1395-minimum-time-visiting-all-points/1395-minimum-time-visiting-all-points.js
@@ -4,16 +4,10 @@
  */
 var minTimeToVisitAllPoints = function(points) {
     let res = 0;
-    let cur = points[0]
-    for(let i=1;i<points.length; i++) {
-        res += Math.max(Math.abs(cur[0] - points[i][0]), Math.abs(cur[1]- points[i][1]))
-        cur = points[i];
+    for(let i=0; i<points.length-1; i++) {
+        const [curX,curY] = points[i]
+        const [nextX, nextY] = points[i+1]
+        res += Math.max(Math.abs(curX - nextX), Math.abs(curY- nextY))
     } 
     return res
-    // function movePoint(cur,goal){
-    //     if(cur === goal) return 0;
-    //     if(cur < goal) return 1;
-    //     return -1;
-    // }
-
 };
