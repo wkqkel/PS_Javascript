3f1006f634d2bc6c9aa02a659b3f755d4404845a
Sangwon Park <wkqkel@naver.com>
2023-09-16 23:20:14 +0900

    Time: 54 ms (67.03%), Space: 41.8 MB (86.81%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.js
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.js
@@ -3,14 +3,25 @@
  * @return {number}
  */
 var pivotInteger = function(n) {
-    const arr = Array(n).fill(1).map((v,i)=>v+i)
-    const sum = (arr) => arr.reduce((acc,cur)=> acc+cur)
-    for(let i =0; i<n; i++) {
-        const sum1 =sum(arr.slice(0, i+1))
-        const sum2 = sum(arr.slice(i))
-        if(sum1 === sum2){
-            return i+1
-        }
+    // const arr = Array(n).fill(1).map((v,i)=>v+i)
+    // const sum = (arr) => arr.reduce((acc,cur)=> acc+cur)
+    // for(let i =0; i<n; i++) {
+    //     const sum1 =sum(arr.slice(0, i+1))
+    //     const sum2 = sum(arr.slice(i))
+    //     if(sum1 === sum2){
+    //         return i + 1
+    //     }
+    // }
+    // return -1
+
+    const sum = (1 + n) * n /2;
+    let sum1 = 0
+    for(let i=1; i<=n; i++) {
+       sum1 += i;
+       const sum2 = sum - sum1 + i;
+       if(sum1 === sum2) {
+           return i
+       }
     }
     return -1
 };
