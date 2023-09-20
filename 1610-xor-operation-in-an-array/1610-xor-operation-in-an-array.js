26b35eb00255a915a107d4cd14eb2575a103d1f4
Sangwon Park <wkqkel@naver.com>
2023-09-21 00:06:00 +0900

    Time: 58 ms (15.69%), Space: 41.8 MB (55.39%) - LeetHub

--- 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.js
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.js
@@ -4,5 +4,6 @@
  * @return {number}
  */
 var xorOperation = function(n, start) {
-    return Array(n).fill(0).map((v,i)=> start + 2* i).reduce((acc,cur)=> acc^cur)
+    // .map((v,i)=> start + 2* i)
+    return Array(n).fill(start).reduce((acc,cur,i)=> acc ^ (start + 2 * i))
 };
