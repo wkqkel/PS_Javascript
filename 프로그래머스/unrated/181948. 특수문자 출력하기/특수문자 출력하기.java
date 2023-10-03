1bc4fd7457d80c4cef45adbbd1c30789facab16e
Sangwon Park <wkqkel@naver.com>
2023-10-04 02:29:31 +0900

    [unrated] Title: 특수문자 출력하기, Time: 112.57 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
