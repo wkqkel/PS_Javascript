be109084a7970d0e0e04dd3b8f5ad0d11f5bd634
Sangwon Park <wkqkel@naver.com>
2023-10-09 02:45:58 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -5,25 +5,12 @@ class Solution {
     public String solution(String code) {
       for(int i = 0; i<code.length(); i++) {
           char cur = code.charAt(i);
-          switch(mode) {
-              case 0: 
-                  if(cur=='1') {
-                      switchMode();
-                      break;
-                  }
-                  if(i % 2== 0) {
-                    ret += cur;   
-                  }
-                  break;
-              case 1:
-                  if(cur == '1') {
-                      switchMode();
-                      break;
-                  }
-                  if(i % 2 != 0) {
-                     ret += cur;
-                  }
-                  break;
+          if(cur=='1') {
+            switchMode();
+            continue;
+          }
+          if(i % 2 == mode) {
+             ret += cur;  
           }          
       }
         
