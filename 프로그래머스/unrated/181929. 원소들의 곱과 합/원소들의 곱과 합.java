b7c65146ad6b4e827074354fb193e2a4f2b22968
Sangwon Park <wkqkel@naver.com>
2023-10-09 02:30:08 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -2,14 +2,23 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] num_list) {
-        int sum = Arrays
-                        .stream(num_list)
-                        .reduce(0, (acc,cur)-> acc+cur);
-        System.out.println(sum);
-        int multiplySum = Arrays
-                        .stream(num_list)
-                        .reduce(1, (acc,cur)-> acc *cur);
-        
-        return multiplySum < Math.pow(sum, 2) ? 1 : 0;
+       int sum = 0;
+       int mul = 1;
+       for(int num: num_list){
+           sum += num;
+           mul *= num;
+       }
+       return mul < Math.pow(sum, 2) ? 1 : 0;
     }
+//     public int solution(int[] num_list) {
+//         int sum = Arrays
+//                         .stream(num_list)
+//                         .sum();
+//         System.out.println(sum);
+//         int multiplySum = Arrays
+//                         .stream(num_list)
+//                         .reduce(1, (acc,cur)-> acc *cur);
+        
+//         return multiplySum < Math.pow(sum, 2) ? 1 : 0;
+//     }
 }
