8c753afdde7ba9e568586acc9047bde02b4367ed
Sangwon Park <wkqkel@naver.com>
2023-10-09 04:03:53 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 6.77 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int limit = 1000000;
+    
+    public int[] solution(int[] arr, int[][] queries) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int[] query: queries){
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            int min = limit;
+            for(int i = 0; i < arr.length; i++) {
+                int cur = arr[i];
+                if(s<= i && i <= e && cur>k) {
+                    min = Math.min(min, cur);
+                }
+            }
+            if(min == limit) min = -1;
+            
+            answer.add(min);
+        }
+        return  answer.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+}
