50c087ab5cabbcfde4ad5a6473d5af0bb1e77a54
Sangwon Park <wkqkel@naver.com>
2023-10-09 04:26:06 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,21 +1,19 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        if(a==b && b== c) {
-            int answer = 1;
-            for(int i = 1; i <= 3; i++){
-               answer *= Math.pow(a, i) + Math.pow(b,i) + Math.pow(c,i);
-            }
-            return answer;
+        int count = 1;
+        int answer = 1;
+        
+        if(a==b && b==c) {
+          count++;
+        }
+        if(a==b || b==c || a==c) {
+          count++;
         }
         
-        if((a==b && b!=c) || (a==c && a!=b) || (b==c && a!=b)) {
-            int answer = 1;
-            for(int i = 1; i <= 2; i++){
-               answer *= Math.pow(a, i) + Math.pow(b,i) + Math.pow(c,i);
-            }
-            return answer;
+        for(int i = 1; i <= count; i++){
+          answer *= Math.pow(a, i) + Math.pow(b,i) + Math.pow(c,i);
         }
         
-        return a+b+c;
+        return answer;
     }
 }
