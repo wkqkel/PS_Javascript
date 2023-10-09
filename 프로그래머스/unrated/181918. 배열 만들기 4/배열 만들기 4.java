a7a7ad9e6f74b76a3f69eac236d386593ac43aff
Sangwon Park <wkqkel@naver.com>
2023-10-10 01:32:16 +0900

    [level unrated] Title: 배열 만들기 4, Time: 4.41 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,29 +1,46 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
+    public Stack<Integer> solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
         int i = 0;
         
         while(i < arr.length) {
-            i = perform(i, stk, arr);
+            if(stack.isEmpty() || stack.peek() < arr[i]) {
+                stack.push(arr[i]);
+                i++;
+            }else if(stack.peek() >= arr[i]) {
+                stack.pop();
+            }
         }
         
-        return toArray(stk);
+        return stack;
     }
     
-    private int perform(int i, ArrayList<Integer> stk, int[] arr) {
-        if(stk.size() == 0 || (stk.size() > 0 && stk.get(stk.size() - 1) < arr[i])){
-            stk.add(arr[i]);
-            return i + 1;
-        }
-        if(stk.size() > 0  && stk.get(stk.size() - 1) >= arr[i]){
-            stk.remove(stk.size() -1);
-        }
-        return i;
-    }
+
+//     public int[] solution(int[] arr) {
+//         ArrayList<Integer> stk = new ArrayList<>();
+//         int i = 0;
+        
+//         while(i < arr.length) {
+//             i = perform(i, stk, arr);
+//         }
+        
+//         return toArray(stk);
+//     }
     
-    private int[] toArray(ArrayList<Integer> arrayList){
-        return arrayList.stream().mapToInt(Integer::intValue).toArray();
-    }
+//     private int perform(int i, ArrayList<Integer> stk, int[] arr) {
+//         if(stk.size() == 0 || (stk.size() > 0 && stk.get(stk.size() - 1) < arr[i])){
+//             stk.add(arr[i]);
+//             return i + 1;
+//         }
+//         if(stk.size() > 0  && stk.get(stk.size() - 1) >= arr[i]){
+//             stk.remove(stk.size() -1);
+//         }
+//         return i;
+//     }
+    
+//     private int[] toArray(ArrayList<Integer> arrayList){
+//         return arrayList.stream().mapToInt(Integer::intValue).toArray();
+//     }
 }
