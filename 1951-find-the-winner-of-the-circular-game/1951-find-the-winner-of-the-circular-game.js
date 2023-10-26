391755181dc0437694506faf1c67c55d86c00a3c
Sangwon Park <wkqkel@naver.com>
2023-10-27 03:50:46 +0900

    Time: 281 ms (5.15%), Space: 48.1 MB (13.23%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.js
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.js
@@ -4,14 +4,23 @@
  * @return {number}
  */
 var findTheWinner = function(n, k) {
+    // const arr = new Array(n).fill(1).map((v,i)=>v+i);
+    // let pos = 0;
+
+    // while(arr.length > 1) {
+    //     pos += k -1;
+    //     if(pos>= arr.length) pos %= arr.length;
+    //     arr.splice(pos,1);
+    // }
+
+    // return arr[0]
+
+    // 풀이2
     const arr = new Array(n).fill(1).map((v,i)=>v+i);
-    let pos = 0;
+    while(arr.length) {
+        if(arr.length === 1) return arr[0]
 
-    while(arr.length > 1) {
-        pos += k -1;
-        if(pos>= arr.length) pos %= arr.length;
-        arr.splice(pos,1);
+        for(let i = 1; i<k; i++) arr.push(arr.shift());
+        arr.shift();
     }
-
-    return arr[0]
 };
