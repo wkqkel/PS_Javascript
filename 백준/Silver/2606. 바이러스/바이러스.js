656fb7d0d4caa89a97cba834779591049a60dba4
Sangwon Park <wkqkel@naver.com>
2023-11-05 19:16:43 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 9488 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,23 +1,56 @@
-let fs = require("fs");
-let input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+/** 
+1. 아이디어
+- 그래프 만들고
+- 큐에 1번을 넣고
+- 1번 방문 처리
+- while에서 
+  - 큐빼내면서
+  - 빼낸 것과 연결된 컴퓨터(노드)들 배열을 for문 돌면서
+  - 방문하지않았으면 방문처리 + 큐에 넣음
+- visited에서 filter true의 갯수
+2. 시간복잡도
+- BFS: O(V+E)
+- V: 최대 100
+- E: 100 * 100
+- 10100 < 2억이하 >> 가능
 
-const n = +input[0]
-let graph = Array(n+1).fill().map(v=>[]);
+3. 자료구조
+- graph: int[][]
+- visited: int[]
+- queue: int[]
+*/
 
-for (let i= 2; i<2+ +input[1]; i++) {
-  const [a,b] = input[i].split(' ').map(Number);
-  graph[a] = [...graph[a] ,b];
-  graph[b] = [...graph[b] ,a];
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+
+const [V,E] = input.slice(0,2).map(Number)
+
+let graph = Array.from({length:V+1}, ()=> Array.from({length:0}));
+
+for(let i = 2; i<input.length; i++){
+  const [a,b] = input[i].split(' ').map(Number)
+  graph[a] = [...graph[a], b]
+  graph[b] = [...graph[b], a]
 }
-const visited = Array(n+1).fill(false)
-function dfs(n,graph,visited){
-  if(!visited[n]) {
-    visited[n] = true;
-    for(let x of graph[n]){
-      dfs(x,graph,visited);
-    }    
+
+let visited = Array.from({length:V+1}, ()=> false)
+let q = [1]
+visited[1] =true;
+let res = 0;
+
+while(q.length) {
+  const v = q.shift();
+  const arr = graph[v];
+  for(let i = 0; i<arr.length;i++) {
+    if(!visited[arr[i]]) {
+      res++
+      visited[arr[i]] = true;
+      q.push(arr[i])
+    }
   }
 }
 
-dfs(1,graph,visited)
-console.log(visited.filter(Boolean).length- 1)
+console.log(res)
+// 1번과 연결된 것이니 1번빼고
+// console.log(visited.filter(Boolean).length - 1)
+
