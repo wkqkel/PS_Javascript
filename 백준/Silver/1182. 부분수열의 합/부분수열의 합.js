d5c569c5927968ea185f7a1ce2565600e533a1c8
Sangwon Park <wkqkel@naver.com>
2023-11-16 01:02:17 +0900

    [Silver II] Title: 부분수열의 합, Time: 184 ms, Memory: 9808 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -1,30 +1,48 @@
+
 const fs = require('fs');
 const input = fs.readFileSync('dev/stdin').toString().trim().split('\n')
 
 const [n,s] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number)
-const ch = []
-const tmp = []
-let cnt = 0;
 
-function recur(L,prev,sum){
-  if(tmp.length && sum === s) {
-    cnt++
-  }
-  if(L === n) {
-    return
-  }
+// 방법1 : 내풀이
+// const ch = []
+// const tmp = []
+// let cnt = 0;
 
-  for(let i=prev; i<n;i++){
-    if(ch[i]) continue;
-    ch[i] = true
-    tmp.push(arr[i])
-    recur(L+1,i,sum+arr[i]);
-    tmp.pop()
-    ch[i] = false
+// function recur(L,prev,sum){
+//   if(tmp.length && sum === s) {
+//     cnt++
+//   }
+//   if(L === n) {
+//     return
+//   }
+
+//   for(let i=prev; i<n;i++){
+//     if(ch[i]) continue;
+//     ch[i] = true
+//     tmp.push(arr[i])
+//     recur(L+1,i,sum+arr[i]);
+//     tmp.pop()
+//     ch[i] = false
+//   }
+// }
+
+// recur(0,0,0)
+// console.log(cnt)
+
+// 방법2: 강의풀이
+// 현재값을 더할지 말지의 경우의 수로 나뉨.
+let res = 0
+function func(cur,tot){
+  if(cur === n) {
+    if(tot === s) res++
+    return;
   }
+  
+  func(cur+1,tot);
+  func(cur+1,tot+arr[cur])
 }
 
-recur(0,0,0)
-console.log(cnt)
-
+func(0,0)
+console.log(s===0 ? res - 1: res)
