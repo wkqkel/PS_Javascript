17529f21efe0663c0d40e6c7065b7ca61570848f
Sangwon Park <wkqkel@naver.com>
2023-12-08 19:34:06 +0900

    [Silver II] Title: 에디터, Time: 68 ms, Memory: 21020 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  string init;
+  cin >> init;
+
+  list<char> L;
+  for(auto c:init) L.push_back(c);
+  auto cursor = L.end();
+
+  int q;
+  cin >> q;
+  while(q--){
+    char op;
+    cin >> op;
+    if(op == 'P'){
+      char add;
+      cin >> add;
+      L.insert(cursor, add);
+    } 
+    else if(op == 'L') {
+      if(cursor != L.begin()) cursor--;
+    }
+    else if(op == 'D') {
+      if(cursor != L.end()) cursor++;
+    }
+    else {
+      if(cursor != L.begin()){
+        cursor--;
+        cursor = L.erase(cursor);
+      }
+    }
+  }
+  for(auto c:L) cout << c;
+}
